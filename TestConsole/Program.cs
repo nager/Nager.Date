b76113dc772abd3693fb20e911a139933358dae4
Tino Hager <tino.hager@nager.at>
2014-08-15 10:31:23 +0200

    start new project

---
+++ TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var publicHolidays = Nager.Date.DateSystem.GetPublicHoliday("DE", 2015);
+
+            var isTodayPublicHoliday = Nager.Date.DateSystem.IsPublicHoliday(DateTime.Now, "AT");
+
+            foreach (var publicHoliday in publicHolidays)
+            {
+                Console.WriteLine("{0:dd.MM.yyyy} {1}", publicHoliday.Date, publicHoliday.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
