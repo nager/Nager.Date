3c034d85c1e6b7703c3039111d6663a906a21dc3
Tino Hager <tino.hager@nager.at>
2017-01-08 19:12:28 +0100

    CountryCode enum add description

--- Nager.Date/CountryCode.cs
+++ Nager.Date/CountryCode.cs
@@ -1,34 +1,61 @@
-﻿namespace Nager.Date
+﻿using System.ComponentModel;
+
+namespace Nager.Date
 {
     /// <summary>
     /// Supported Country Codes
     /// </summary>
     public enum CountryCode
     {
+        [Description("Austria")]
         AT,
+        [Description("Belgium")]
         BE,
+        [Description("Bulgaria")]
         BG,
+        [Description("Switzerland")]
         CH,
+        [Description("Czech Republic")]
         CZ,
+        [Description("Germany")]
         DE,
+        [Description("Denmark")]
         DK,
+        [Description("Estonia")]
         EE,
+        [Description("Spain")]
         ES,
+        [Description("Finland")]
         FI,
+        [Description("France")]
         FR,
+        [Description("Croatia")]
         HR,
+        [Description("Ireland")]
         IE,
+        [Description("Italy")]
         IT,
+        [Description("Liechtenstein")]
         LI,
+        [Description("Luxembourg")]
         LU,
+        [Description("Latvia")]
         LV,
+        [Description("Malta")]
         MT,
+        [Description("Netherlands")]
         NL,
+        [Description("Poland")]
         PL,
+        [Description("Portugal")]
         PT,
+        [Description("Romania")]
         RO,
+        [Description("Sweden")]
         SE,
+        [Description("Slovenia")]
         SI,
+        [Description("United Kingdom")]
         UK,
     }
 }
