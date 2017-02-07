b3c6bd9b75eb527f435f5326a5718b1e0afcac97
stemih333 <stemih11@gmail.com>
2017-02-07 11:47:18 +0100

    Update SwedenProvider.cs
    Added swedish public holidays "New Year's Eve", "Christmas Eve" and "Midsummer Eve". 
    
    Ref http://www.officeholidays.com/countries/sweden/

--- Nager.Date/PublicHolidays/SwedenProvider.cs
+++ Nager.Date/PublicHolidays/SwedenProvider.cs
@@ -26,10 +26,13 @@ namespace Nager.Date.PublicHolidays
             items.Add(new PublicHoliday(year, 5, 1, "Första maj", "International Workers' Day", countryCode));
             items.Add(new PublicHoliday(easterSunday.AddDays(39), "Kristi himmelsfärds dag", "Ascension Day", countryCode));
             items.Add(new PublicHoliday(year, 6, 6, "Sveriges nationaldag", "National Day of Sweden", countryCode));
+            items.Add(new PublicHoliday(midsummerDay.AddDays(-1), "midsommarafton", "Midsummer Eve", countryCode));
             items.Add(new PublicHoliday(midsummerDay, "midsommardagen", "Midsummer Day", countryCode));
             items.Add(new PublicHoliday(allSaintsDay, "alla helgons dag", "All Saints' Day", countryCode));
+            items.Add(new PublicHoliday(year, 12, 24, "julafton", "Christmas Eve", countryCode));
             items.Add(new PublicHoliday(year, 12, 25, "juldagen", "Christmas Day", countryCode));
             items.Add(new PublicHoliday(year, 12, 26, "annandag jul", "St. Stephen's Day", countryCode));
+            items.Add(new PublicHoliday(year, 12, 31, "nyårsafton", "New Year's Eve", countryCode));
 
             return items.OrderBy(o => o.Date);
         }
