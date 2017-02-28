77493d547f91aab485d85d0b2692b8ac8bb930f4
Tino Hager <tino.hager@nager.at>
2017-02-28 22:31:36 +0100

    namespace cleanup

--- Nager.Date/PublicHolidays/OrthodoxBaseProvider.cs
+++ Nager.Date/PublicHolidays/OrthodoxBaseProvider.cs
@@ -2,9 +2,6 @@
 using Nager.Date.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nager.Date.PublicHolidays
 {
