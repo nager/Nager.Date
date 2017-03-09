9694e62fc41a1da366d1de188a87c4948137250b
Tino Hager <tino.hager@nager.at>
2017-03-09 18:52:25 +0100

    Add UnitTest for check relation with country codes

---
+++ Nager.Date.UnitTest/UnitTestRelation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Nager.Date.UnitTest
+{
+    [TestClass]
+    public class UnitTestRelation
+    {
+        [TestMethod]
+        public void TestAllRelationsExist()
+        {
+            foreach (CountryCode countryCode in Enum.GetValues(typeof(CountryCode)))
+            {
+                var items = DateSystem.GetPublicHoliday(countryCode, DateTime.UtcNow.Year).ToList();
+                Assert.AreNotEqual(0, items.Count);
+            }
+        }
+    }
+}
