85442cc35af0f08f56fa443e688e05a1ce595557
Jeremy <jeremykentc@gmail.com>
2017-10-03 16:04:20 -0400

    adding PR unit tests

---
+++ Src/Nager.Date.UnitTest/PuertoRicoTest.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nager.Date.UnitTest
+{
+    [TestClass]
+    public class PuertoRicoTest
+    {
+
+        [TestMethod]
+        public void PuertoRicoHasGoodFridayHoliday()
+        {
+            var holidays = DateSystem.GetPublicHoliday(CountryCode.PR, 2017);
+            
+            var catholic = new MockProvider();
+            var expectedGoodFriday = catholic.EasterSunday(2017).AddDays(-2);
+
+            var goodFriday = holidays.First(h => h.Name == "Good Friday");
+            Assert.IsNotNull(goodFriday);
+            Assert.AreEqual(expectedGoodFriday.Day, goodFriday.Date.Day);
+        }
+    }
+}
