43d2095c150d7edd23faafcff248edf05441f130
Semen Miroshnichenko <semenmiroshnichenko@gmail.com>
2017-10-10 22:02:08 +0200

    Fix translation for International Women's Day

--- Src/Nager.Date/PublicHolidays/RussiaProvider.cs
+++ Src/Nager.Date/PublicHolidays/RussiaProvider.cs
@@ -23,7 +23,7 @@ namespace Nager.Date.PublicHolidays
             items.Add(new PublicHoliday(year, 1, 6, "Новогодние каникулы", "New Year holiday", countryCode));
             items.Add(new PublicHoliday(year, 1, 7, "Рождество Христово", "Orthodox Christmas Day", countryCode));
             items.Add(new PublicHoliday(year, 2, 23, "День защитника Отечества", "Defender of the Fatherland Day", countryCode, 1918));
-            items.Add(new PublicHoliday(year, 3, 8, "Мiжнародны жаночы дзень", "International Women's Day", countryCode, 1913));
+            items.Add(new PublicHoliday(year, 3, 8, "Международный женский день", "International Women's Day", countryCode, 1913));
             items.Add(new PublicHoliday(year, 5, 1, "День труда", "Labour Day", countryCode));
             items.Add(new PublicHoliday(year, 5, 9, "День Победы", "Victory Day", countryCode));
             items.Add(new PublicHoliday(year, 6, 12, "День России", "Russia Day", countryCode, 2002));
