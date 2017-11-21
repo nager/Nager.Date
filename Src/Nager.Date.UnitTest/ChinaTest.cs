a8bd0c703f952037cdd88d2634380e030faf3958
Tino Hager <tino.hager@nager.at>
2017-11-21 23:41:24 +0100

    Optimize China Provider

---
+++ Src/Nager.Date.UnitTest/ChinaTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nager.Date.UnitTest
+{
+    [TestClass]
+    public class ChinaTest
+    {
+        [TestMethod]
+        public void TestChina2015()
+        {
+            var publicHolidays = DateSystem.GetPublicHoliday(CountryCode.CN, 2015).ToArray();
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2015, 9, 27) && o.Name == "Mid-Autumn Festival").Any());
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2015, 4, 5) && o.Name == "Qingming Festival (Tomb-Sweeping Day)").Any());
+        }
+
+        [TestMethod]
+        public void TestChina2016()
+        {
+            var publicHolidays = DateSystem.GetPublicHoliday(CountryCode.CN, 2016).ToArray();
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2016, 9, 15) && o.Name == "Mid-Autumn Festival").Any());
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2016, 4, 4) && o.Name == "Qingming Festival (Tomb-Sweeping Day)").Any());
+        }
+
+        [TestMethod]
+        public void TestChina2017()
+        {
+            var publicHolidays = DateSystem.GetPublicHoliday(CountryCode.CN, 2017).ToArray();
+            Assert.IsTrue(publicHolidays.Where(o=>o.Date == new DateTime(2017, 10, 4) && o.Name == "Mid-Autumn Festival").Any());
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2017, 4, 4) && o.Name == "Qingming Festival (Tomb-Sweeping Day)").Any());
+        }
+
+        [TestMethod]
+        public void TestChina2018()
+        {
+            var publicHolidays = DateSystem.GetPublicHoliday(CountryCode.CN, 2018).ToArray();
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2018, 9, 24) && o.Name == "Mid-Autumn Festival").Any());
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2018, 4, 5) && o.Name == "Qingming Festival (Tomb-Sweeping Day)").Any());
+        }
+
+        [TestMethod]
+        public void TestChina2019()
+        {
+            var publicHolidays = DateSystem.GetPublicHoliday(CountryCode.CN, 2019).ToArray();
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2019, 9, 13) && o.Name == "Mid-Autumn Festival").Any());
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2019, 4, 5) && o.Name == "Qingming Festival (Tomb-Sweeping Day)").Any());
+        }
+
+        [TestMethod]
+        public void TestChina2020()
+        {
+            var publicHolidays = DateSystem.GetPublicHoliday(CountryCode.CN, 2020).ToArray();
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2020, 10, 1) && o.Name == "Mid-Autumn Festival").Any());
+            Assert.IsTrue(publicHolidays.Where(o => o.Date == new DateTime(2019, 4, 4) && o.Name == "Qingming Festival (Tomb-Sweeping Day)").Any());
+        }
+    }
+}
--- Src/Nager.Date/PublicHolidays/ChinaProvider.cs
+++ Src/Nager.Date/PublicHolidays/ChinaProvider.cs
@@ -12,7 +12,6 @@ namespace Nager.Date.PublicHolidays
         {
             //TODO: Provider incomplete
             //Tomb-Sweeping-Day is invalid (5th solar term)
-            //Mid-Autumn Festival is invalid for 2017
 
             //China
             //https://en.wikipedia.org/wiki/Public_holidays_in_China
@@ -21,20 +20,36 @@ namespace Nager.Date.PublicHolidays
             var items = new List<PublicHoliday>();
 
             var chineseCalendar = new ChineseLunisolarCalendar();
-            var springFestival = chineseCalendar.ToDateTime(year, 1, 1, 0, 0, 0, 0);
-            var tombSweepimgDay = chineseCalendar.ToDateTime(year, 5, 1, 0, 0, 0, 0);
-            var dragonBoatFestival = chineseCalendar.ToDateTime(year, 5, 5, 0, 0, 0, 0);
-            var midAutumnFestival = chineseCalendar.ToDateTime(year, 8, 15, 0, 0, 0, 0);
+            var leapMonth = chineseCalendar.GetLeapMonth(year);
+            var springFestival = chineseCalendar.ToDateTime(year, this.MoveMonth(1, leapMonth), 1, 0, 0, 0, 0);
+            var tombSweepimgDay = chineseCalendar.ToDateTime(year, this.MoveMonth(4, leapMonth), 5, 0, 0, 0, 0);
+            var dragonBoatFestival = chineseCalendar.ToDateTime(year, this.MoveMonth(5, leapMonth), 5, 0, 0, 0, 0);
+            var midAutumnFestival = chineseCalendar.ToDateTime(year, this.MoveMonth(8, leapMonth), 15, 0, 0, 0, 0);
 
             items.Add(new PublicHoliday(year, 1, 1, "元旦", "New Year's Day", countryCode));
-            items.Add(new PublicHoliday(springFestival, "春节", "Spring Festival", countryCode));
-            //items.Add(new PublicHoliday(tombSweepimgDay, "清明节", "Tomb-Sweeping Day", countryCode));
+            items.Add(new PublicHoliday(springFestival, "春节", "Chinese New Year (Spring Festival)", countryCode));
+            items.Add(new PublicHoliday(tombSweepimgDay, "清明节", "Qingming Festival (Tomb-Sweeping Day)", countryCode));
             items.Add(new PublicHoliday(year, 5, 1, "劳动节", "Labour Day", countryCode));
             items.Add(new PublicHoliday(dragonBoatFestival, "端午节", "Dragon Boat Festival", countryCode));
-            //items.Add(new PublicHoliday(midAutumnFestival, "中秋节", "Mid-Autumn Festival", countryCode));
+            items.Add(new PublicHoliday(midAutumnFestival, "中秋节", "Mid-Autumn Festival", countryCode));
             items.Add(new PublicHoliday(year, 10, 1, "国庆节", "National Day", countryCode));
 
             return items.OrderBy(o => o.Date);
+        }
+
+        private int MoveMonth(int month, int leapMonth)
+        {
+            if (leapMonth == 0)
+            {
+                return month;
+            }
+
+            if (leapMonth < month)
+            {
+                return ++month;
+            }
+
+            return month;
         }
     }
 }
