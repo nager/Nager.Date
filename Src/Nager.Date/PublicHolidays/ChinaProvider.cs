f1073f87ececdd90f8a0a11230a97dbca0ee5de4
Tino Hager <tino.hager@nager.at>
2017-11-26 22:38:34 +0100

    optimize china provider

--- Src/Nager.Date/PublicHolidays/ChinaProvider.cs
+++ Src/Nager.Date/PublicHolidays/ChinaProvider.cs
@@ -22,13 +22,12 @@ namespace Nager.Date.PublicHolidays
             var chineseCalendar = new ChineseLunisolarCalendar();
             var leapMonth = chineseCalendar.GetLeapMonth(year);
             var springFestival = chineseCalendar.ToDateTime(year, this.MoveMonth(1, leapMonth), 1, 0, 0, 0, 0);
-            var tombSweepimgDay = chineseCalendar.ToDateTime(year, this.MoveMonth(4, leapMonth), 5, 0, 0, 0, 0);
             var dragonBoatFestival = chineseCalendar.ToDateTime(year, this.MoveMonth(5, leapMonth), 5, 0, 0, 0, 0);
             var midAutumnFestival = chineseCalendar.ToDateTime(year, this.MoveMonth(8, leapMonth), 15, 0, 0, 0, 0);
 
             items.Add(new PublicHoliday(year, 1, 1, "元旦", "New Year's Day", countryCode));
             items.Add(new PublicHoliday(springFestival, "春节", "Chinese New Year (Spring Festival)", countryCode));
-            items.Add(new PublicHoliday(tombSweepimgDay, "清明节", "Qingming Festival (Tomb-Sweeping Day)", countryCode));
+            items.Add(new PublicHoliday(year, 4, 5, "清明节", "Qingming Festival (Tomb-Sweeping Day)", countryCode)); //TODO: Date is not fixed, calculate from 5th solar term
             items.Add(new PublicHoliday(year, 5, 1, "劳动节", "Labour Day", countryCode));
             items.Add(new PublicHoliday(dragonBoatFestival, "端午节", "Dragon Boat Festival", countryCode));
             items.Add(new PublicHoliday(midAutumnFestival, "中秋节", "Mid-Autumn Festival", countryCode));
