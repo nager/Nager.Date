5bb6c7e9288d7df3eda5db7a28117ba44f9e3110
Tino Hager <tino.hager@nager.at>
2017-12-28 20:42:28 +0100

    Optimize france provider

--- Src/Nager.Date/PublicHolidays/FranceProvider.cs
+++ Src/Nager.Date/PublicHolidays/FranceProvider.cs
@@ -10,6 +10,7 @@ namespace Nager.Date.PublicHolidays
         {
             //France
             //https://en.wikipedia.org/wiki/Public_holidays_in_France
+            //https://en.wikipedia.org/wiki/ISO_3166-2:FR
 
             var countryCode = CountryCode.FR;
             var easterSunday = base.EasterSunday(year);
@@ -21,13 +22,15 @@ namespace Nager.Date.PublicHolidays
             items.Add(new PublicHoliday(year, 5, 1, "Fête du premier mai", "Labour Day", countryCode));
             items.Add(new PublicHoliday(easterSunday.AddDays(39), "Jour de l'Ascension", "Ascension Day", countryCode));
             items.Add(new PublicHoliday(year, 5, 8, "Fête de la Victoire", "Victory in Europe Day", countryCode));
+            items.Add(new PublicHoliday(year, 5, 22, "Abolition de l'esclavage", "Abolition of Slavery", countryCode, null, new string[] { "FR-MQ" }));
+            items.Add(new PublicHoliday(year, 5, 27, "Abolition of Slavery", "Abolition de l'esclavage", countryCode, null, new string[] { "FR-GP", "FR-MF", "FR-BL" }));
             items.Add(new PublicHoliday(easterSunday.AddDays(50), "Lundi de Pentecôte", "Whit Monday", countryCode));
             items.Add(new PublicHoliday(year, 7, 14, "Fête nationale", "Bastille Day", countryCode));
             items.Add(new PublicHoliday(year, 8, 15, "L'Assomption de Marie", "Assumption Day", countryCode));
             items.Add(new PublicHoliday(year, 11, 1, "La Toussaint", "All Saints' Day", countryCode));
             items.Add(new PublicHoliday(year, 11, 11, "Armistice de 1918", "Armistice Day", countryCode));
             items.Add(new PublicHoliday(year, 12, 25, "Noël", "Christmas Day", countryCode));
-            items.Add(new PublicHoliday(year, 12, 26, "Saint-Étienne", "St. Stephen's Day", countryCode));
+            items.Add(new PublicHoliday(year, 12, 26, "Saint-Étienne", "St. Stephen's Day", countryCode, null, new string[] { "FR-A", "FR-54" }));
 
             return items.OrderBy(o => o.Date);
         }
