968ddcae79cef1e1d3340f6d54f55ee68a13e179
kreiden <41423724+kreiden@users.noreply.github.com>
2018-08-06 13:07:18 +0200

    Update Providers

--- Src/Nager.Date/PublicHolidays/SerbiaProvider.cs
+++ Src/Nager.Date/PublicHolidays/SerbiaProvider.cs
@@ -1,9 +1,6 @@
 ﻿using Nager.Date.Model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nager.Date.PublicHolidays
 {
--- Src/Nager.Date/PublicHolidays/TunisiaProvider.cs
+++ Src/Nager.Date/PublicHolidays/TunisiaProvider.cs
@@ -1,10 +1,7 @@
 ﻿using Nager.Date.Contract;
 using Nager.Date.Model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nager.Date.PublicHolidays
 {
