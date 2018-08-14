1704872f311f4af607dea48a25683b825676f641
Jesper Munk <jemunk@gmail.com>
2018-08-14 09:27:10 +0200

    Added Faroe Islands provider

---
+++ Src/Nager.Date/PublicHolidays/FaroeIslandsProvider.cs
@@ -0,0 +1,36 @@
+﻿using Nager.Date.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nager.Date.PublicHolidays
+{
+    public class FaroeIslandsProvider : CatholicBaseProvider
+    {
+        public override IEnumerable<PublicHoliday> Get(int year)
+        {
+            //Faroe Islands, adaptation of DenmarkProvider
+
+            var countryCode = CountryCode.FO;
+            var easterSunday = base.EasterSunday(year);
+
+            var items = new List<PublicHoliday>();
+            items.Add(new PublicHoliday(year, 1, 1, "Nytårsdag", "New Year's Day", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(-3), "Skærtorsdag", "Maundy Thursday", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(-2), "Langfredag", "Good Friday", countryCode));
+            items.Add(new PublicHoliday(easterSunday, "Påskedag", "Easter Sunday", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(1), "2. Påskedag", "Easter Monday", countryCode));
+            items.Add(new PublicHoliday(year, 4, 25, "Flagdag", "Flag day", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(26), "Store bededag", "General Prayer Day", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(39), "Kristi Himmelfartsdag", "Ascension Day", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(49), "Pinsedag", "Pentecost", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(50), "2. Pinsedag", "Whit Monday", countryCode));
+            items.Add(new PublicHoliday(year, 7, 28, "Ólavsøkuaftan", "Olaifesten eve", countryCode));
+            items.Add(new PublicHoliday(year, 7, 29, "Ólavsøkudagur", "Olaifesten", countryCode));
+            items.Add(new PublicHoliday(year, 12, 24, "Juleaften", "Christmas Eve", countryCode));
+            items.Add(new PublicHoliday(year, 12, 25, "Juledag / 1. juledag", "Christmas Day", countryCode));
+            items.Add(new PublicHoliday(year, 12, 26, "2. juledag", "St. Stephen's Day", countryCode));
+
+            return items.OrderBy(o => o.Date);
+        }
+    }
+}
