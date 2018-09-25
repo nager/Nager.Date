9dfd9031ff59b193e9c77b4bd77f100c42cb0ff9
Pontus Anttila <pontus.anttila@mio.se>
2018-09-25 13:25:25 +0200

    add a more complete list of swedish holidays

--- Src/Nager.Date/PublicHolidays/SwedenProvider.cs
+++ Src/Nager.Date/PublicHolidays/SwedenProvider.cs
@@ -19,21 +19,29 @@ namespace Nager.Date.PublicHolidays
             var allSaintsDay = DateSystem.FindDay(year, 10, 31, DayOfWeek.Saturday);
 
             var items = new List<PublicHoliday>();
-            items.Add(new PublicHoliday(year, 1, 1, "nyårsdagen", "New Year's Day", countryCode));
-            items.Add(new PublicHoliday(year, 1, 6, "trettondedag jul", "Epiphany", countryCode));
-            items.Add(new PublicHoliday(easterSunday.AddDays(-2), "långfredagen", "Good Friday", countryCode));
-            items.Add(new PublicHoliday(easterSunday, "Påsk", "Easter Sunday", countryCode));
-            items.Add(new PublicHoliday(easterSunday.AddDays(1), "annandag påsk", "Easter Monday", countryCode));
+
+            items.Add(new PublicHoliday(year, 1, 5, "Trettondagsafton", "Twelth night", countryCode));
+            items.Add(new PublicHoliday(year, 1, 6, "Trettondedag jul", "Epiphany", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(-3), "Skärtorsdagen", "Maundy Thursday", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(-2), "Långfredagen", "Good Friday", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(-1), "Påskafton", "Easter Saturday", countryCode));
+            items.Add(new PublicHoliday(easterSunday, "Påskdagen", "Easter Sunday", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(1), "Annandag påsk", "Easter Monday", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(48), "Pingstafton", "Whitsun Eve", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(49), "Pingstdagen", "Withsun Day", countryCode));
             items.Add(new PublicHoliday(year, 5, 1, "Första maj", "International Workers' Day", countryCode));
-            items.Add(new PublicHoliday(easterSunday.AddDays(39), "Kristi himmelsfärds dag", "Ascension Day", countryCode));
-            items.Add(new PublicHoliday(year, 6, 6, "Sveriges nationaldag", "National Day of Sweden", countryCode));
-            items.Add(new PublicHoliday(midsummerDay.AddDays(-1), "midsommarafton", "Midsummer Eve", countryCode));
-            items.Add(new PublicHoliday(midsummerDay, "midsommardagen", "Midsummer Day", countryCode));
-            items.Add(new PublicHoliday(allSaintsDay, "alla helgons dag", "All Saints' Day", countryCode));
-            items.Add(new PublicHoliday(year, 12, 24, "julafton", "Christmas Eve", countryCode));
-            items.Add(new PublicHoliday(year, 12, 25, "juldagen", "Christmas Day", countryCode));
-            items.Add(new PublicHoliday(year, 12, 26, "annandag jul", "St. Stephen's Day", countryCode));
-            items.Add(new PublicHoliday(year, 12, 31, "nyårsafton", "New Year's Eve", countryCode));
+            items.Add(new PublicHoliday(easterSunday.AddDays(39), "Kristi himmelsfärdsdag", "Ascension Day", countryCode));
+            items.Add(new PublicHoliday(year, 6, 6, "Nationaldagen", "National Day of Sweden", countryCode));
+            items.Add(new PublicHoliday(midsummerDay.AddDays(-1), "Midsommarafton", "Midsummer Eve", countryCode));
+            items.Add(new PublicHoliday(midsummerDay, "Midsommardagen", "Midsummer Day", countryCode));
+            items.Add(new PublicHoliday(allSaintsDay.AddDays(-1), "Allhelgonaafton", "Saint's Eve", countryCode));
+            items.Add(new PublicHoliday(allSaintsDay, "Alla helgons dag", "All Saints' Day", countryCode));
+            items.Add(new PublicHoliday(year, 12, 24, "Julafton", "Christmas Eve", countryCode));
+            items.Add(new PublicHoliday(year, 12, 25, "Juldagen", "Christmas Day", countryCode));
+            items.Add(new PublicHoliday(year, 12, 26, "Annandag jul", "St. Stephen's Day", countryCode));
+            items.Add(new PublicHoliday(year, 1, 1, "Nyårsdagen", "New Year's Day", countryCode));
+            items.Add(new PublicHoliday(year, 12, 31, "Nyårsafton", "New Year's Eve", countryCode));
+            items.Add(new PublicHoliday(year, 4, 30, "Valborgsmässoafton", "Walpurgis night", countryCode));
 
             return items.OrderBy(o => o.Date);
         }
