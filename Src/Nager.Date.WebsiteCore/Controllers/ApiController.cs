a6f90f4fc9b53a3d8a981897c8bf4641fee494aa
Tino Hager <tino.hager@nager.at>
2018-10-22 22:33:16 +0200

    Add old api method

--- Src/Nager.Date.WebsiteCore/Controllers/ApiController.cs
+++ Src/Nager.Date.WebsiteCore/Controllers/ApiController.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nager.Date.WebsiteCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Nager.Date.WebsiteCore.Controllers
 {
     [Route("[controller]")]
+    //[ApiController]
     public class ApiController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [Route("get/{countrycode}/{year}")]
+        public ActionResult<IEnumerable<PublicHoliday>> CountryJson(string countrycode, int year)
+        {
+            //Counter++;
+
+            if (!Enum.TryParse(countrycode, true, out CountryCode countryCode))
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+
+            var publicHolidays = DateSystem.GetPublicHoliday(countryCode, year);
+            if (publicHolidays?.Count() > 0)
+            {
+                var items = publicHolidays.Where(o => o.Type.HasFlag(Model.PublicHolidayType.Public)).Select(o => new PublicHoliday(o));
+                return StatusCode(StatusCodes.Status200OK, items);
+            }
+
+            return StatusCode(StatusCodes.Status404NotFound);
         }
     }
 }
