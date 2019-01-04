0f0955671352d42d21ff4149f73c77108eda8da2
Tino Hager <tino.hager@nager.at>
2019-01-04 22:57:57 +0100

    Add Sitemap logic

--- Src/Nager.Date.WebsiteCore/Controllers/SitemapController.cs
+++ Src/Nager.Date.WebsiteCore/Controllers/SitemapController.cs
@@ -1,12 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleMvcSitemap;
+using System;
+using System.Collections.Generic;
 
 namespace Nager.Date.WebsiteCore.Controllers
 {
     public class SitemapController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string countryCode)
         {
-            return View();
+            #region Country Overview
+
+            if (string.IsNullOrEmpty(countryCode))
+            {
+                var indexNodes = new List<SitemapIndexNode>();
+                var items = Enum.GetValues(typeof(CountryCode));
+                foreach (var item in items)
+                {
+                    indexNodes.Add(new SitemapIndexNode(Url.Action("Index", new { countryCode = item.ToString() })));
+                }
+
+                return new SitemapProvider().CreateSitemapIndex(new SitemapIndexModel(indexNodes));
+            }
+
+            #endregion
+
+            #region Country Detail
+
+            var startYear = DateTime.Today.Year - 100;
+            var endYear = DateTime.Today.Year + 100;
+
+            var nodes = new List<SitemapNode>();
+            for (var i = startYear; i < endYear; i++)
+            {
+                nodes.Add(new SitemapNode(Url.Action("Country", "PublicHoliday", new { countryCode = countryCode.ToString(), year = i })) { ChangeFrequency = ChangeFrequency.Monthly });
+            }
+
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+
+            #endregion
         }
     }
 }
