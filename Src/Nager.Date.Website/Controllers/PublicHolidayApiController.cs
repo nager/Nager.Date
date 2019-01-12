27a14079b0c0232b2d8a6f3cd3032d23721e43d4
Tino Hager <tino.hager@nager.at>
2019-01-12 15:46:40 +0100

    Optimize statistic

--- Src/Nager.Date.Website/Controllers/PublicHolidayApiController.cs
+++ Src/Nager.Date.Website/Controllers/PublicHolidayApiController.cs
@@ -1,8 +1,10 @@
 ﻿using Nager.Date.Website.Model;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Web;
 using System.Web.Http;
 
 namespace Nager.Date.Website.Controllers
@@ -10,20 +12,21 @@ namespace Nager.Date.Website.Controllers
     [RoutePrefix("api/v1")]
     public class PublicHolidayApiController : ApiController
     {
-        private static int Counter = 0;
+        private static ConcurrentDictionary<string, int> ApiStatistic = new ConcurrentDictionary<string, int>();
 
         [HttpGet]
         [Route("statistic")]
-        public int Statistic()
+        public KeyValuePair<string, int>[] Statistic()
         {
-            return Counter;
+            return ApiStatistic.Select(o => new KeyValuePair<string, int>(o.Key, o.Value)).ToArray();
         }
 
         [HttpGet]
         [Route("get/{countrycode}/{year}")]
         public IEnumerable<PublicHoliday> CountryJson(string countrycode, int year)
         {
-            Counter++;
+            var ipAddress = HttpContext.Current.Request.UserHostAddress;
+            ApiStatistic.AddOrUpdate(ipAddress, 1, (s, i) => i++);
 
             CountryCode countryCode;
             if (!Enum.TryParse(countrycode, true, out countryCode))
