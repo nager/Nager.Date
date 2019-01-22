bc49a1a8656190876bc16f6c43ef587d7cca336e
Tino Hager <tino.hager@nager.at>
2019-01-23 00:06:15 +0100

    Add summary to long weekend

--- Src/Nager.Date/Model/LongWeekend.cs
+++ Src/Nager.Date/Model/LongWeekend.cs
@@ -2,13 +2,33 @@
 
 namespace Nager.Date.Model
 {
+    /// <summary>
+    /// LongWeekend
+    /// The best long weekends  
+    /// </summary>
     public class LongWeekend
     {
+        /// <summary>
+        /// Start date
+        /// </summary>
         public DateTime StartDate { get; set; }
+        /// <summary>
+        /// End date
+        /// </summary>
         public DateTime EndDate { get; set; }
+        /// <summary>
+        /// Count of days
+        /// </summary>
         public int DayCount { get { return this.EndDate.Subtract(this.StartDate).Days + 1; } }
+        /// <summary>
+        /// If a additional holiday needed
+        /// </summary>
         public bool Bridge { get; set; }
 
+        /// <summary>
+        /// ToString
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"{this.StartDate} - {this.EndDate} ({this.DayCount})";
