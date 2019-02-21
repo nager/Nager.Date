3b86c2bf8569fbf902d8792c1a17092c2f329146
Tino Hager <tino.hager@nager.at>
2019-02-21 22:35:08 +0100

    Optimize website

--- Src/Nager.Date.WebsiteCore/Models/PublicHolidayDto.cs
+++ Src/Nager.Date.WebsiteCore/Models/PublicHolidayDto.cs
@@ -7,17 +7,41 @@ namespace Nager.Date.WebsiteCore.Models
 {
     public class PublicHolidayDto
     {
+        /// <summary>
+        /// The date
+        /// </summary>
         public DateTime Date { get; set; }
+        /// <summary>
+        /// Local name
+        /// </summary>
         public string LocalName { get; set; }
+        /// <summary>
+        /// English name
+        /// </summary>
         public string Name { get; set; }
-        //ISO 3166-1 alpha-2
+        /// <summary>
+        /// ISO 3166-1 alpha-2
+        /// </summary>
         public string CountryCode { get; set; }
+        /// <summary>
+        /// Is this public holiday every year on the same date
+        /// </summary>
         public bool Fixed { get; set; }
+        /// <summary>
+        /// Is this public holiday in every county (federal state)
+        /// </summary>
         public bool Global { get; set; }
-        //ISO_3166-2
+        /// <summary>
+        /// ISO-3166-2 - Federal states
+        /// </summary>
         public string[] Counties { get; set; }
+        /// <summary>
+        /// The launch year of the public holiday
+        /// </summary>
         public int? LaunchYear { get; set; }
-
+        /// <summary>
+        /// A list of types the public holiday it is valid
+        /// </summary>
         [JsonConverter(typeof(StringEnumConverter))]
         public PublicHolidayType Type { get; set; }
     }
