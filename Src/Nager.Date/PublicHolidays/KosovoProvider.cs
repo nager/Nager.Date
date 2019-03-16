984e06b6a6d258f2450a9304ebfdfe7427f7a532
Tino Hager <tino.hager@nager.at>
2019-03-16 10:02:55 +0100

    cleanup

--- Src/Nager.Date/PublicHolidays/KosovoProvider.cs
+++ Src/Nager.Date/PublicHolidays/KosovoProvider.cs
@@ -1,10 +1,7 @@
 ﻿using Nager.Date.Contract;
 using Nager.Date.Model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nager.Date.PublicHolidays
 {
@@ -48,8 +45,8 @@ namespace Nager.Date.PublicHolidays
             items.Add(new PublicHoliday(orthodoxEasterSunday, "Pashkët Ortodokse", "Orthodox Easter", countryCode));
             items.Add(new PublicHoliday(year, 5, 1, "Dita Ndërkombëtare e Punës", "International Workers' Day", countryCode));
             items.Add(new PublicHoliday(year, 5, 9, "Dita e Evropës", "Europe Day", countryCode));
-            //Eid ul-Fitr is not implemented
-            //Eid ul-Adha is not implemented
+            //TODO:Eid ul-Fitr is not implemented
+            //TODO:Eid ul-Adha is not implemented
             items.Add(new PublicHoliday(year, 12, 25, "Krishtlindjet Katolike", "Catholic Christmas", countryCode));
 
             return items.OrderBy(o => o.Date);
