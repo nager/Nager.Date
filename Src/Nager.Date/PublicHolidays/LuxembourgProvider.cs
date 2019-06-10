96111b87f07717f319b4f80829fe3384e8091541
Tino Hager <tino.hager@nager.at>
2019-06-10 20:43:41 +0200

    Add Launchyear

--- Src/Nager.Date/PublicHolidays/LuxembourgProvider.cs
+++ Src/Nager.Date/PublicHolidays/LuxembourgProvider.cs
@@ -35,7 +35,7 @@ namespace Nager.Date.PublicHolidays
             items.Add(new PublicHoliday(year, 1, 1, "Neijoerschdag", "New Year's Day", countryCode));
             items.Add(new PublicHoliday(easterSunday.AddDays(1), "Ouschterméindeg", "Easter Monday", countryCode));
             items.Add(new PublicHoliday(year, 5, 1, "Dag vun der Aarbecht", "Labour Day", countryCode));
-            items.Add(new PublicHoliday(year, 5, 9, "Europadag", "Europe Day", countryCode));
+            items.Add(new PublicHoliday(year, 5, 9, "Europadag", "Europe Day", countryCode, 2019));
             items.Add(new PublicHoliday(easterSunday.AddDays(-2), "Karfreideg", "Good Friday", countryCode, type: PublicHolidayType.Bank));
             items.Add(new PublicHoliday(easterSunday.AddDays(39), "Christi Himmelfaart", "Ascension Day", countryCode));
             items.Add(new PublicHoliday(easterSunday.AddDays(50), "Péngschtméindeg", "Whit Monday", countryCode));
