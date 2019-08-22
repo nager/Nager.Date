0366a50820eafcf20919d24e22f5c1cccf8819c9
Tino Hager <tino.hager@nager.at>
2019-08-22 21:52:14 +0200

    Corrected Czech and Slovak holidays
    https://github.com/tinohager/Nager.Date/issues/169

--- Src/Nager.Date/PublicHolidays/CzechRepublicProvider.cs
+++ Src/Nager.Date/PublicHolidays/CzechRepublicProvider.cs
@@ -34,15 +34,14 @@ namespace Nager.Date.PublicHolidays
             var items = new List<PublicHoliday>();
             items.Add(new PublicHoliday(year, 1, 1, "Den obnovy samostatného českého státu; Nový rok", "New Year's Day", countryCode));
             items.Add(new PublicHoliday(easterSunday.AddDays(-2), "Velký pátek", "Good Friday", countryCode, 2016));
-            items.Add(new PublicHoliday(easterSunday, "Velikonoční neděle", "Easter Sunday", countryCode));
             items.Add(new PublicHoliday(easterSunday.AddDays(1), "Velikonoční pondělí", "Easter Monday", countryCode));
             items.Add(new PublicHoliday(year, 5, 1, "Svátek práce", "Labour Day", countryCode));
-            items.Add(new PublicHoliday(year, 5, 8, "Den vítězství - Den osvobození od fašismu", "Liberation Day", countryCode));
+            items.Add(new PublicHoliday(year, 5, 8, "Den vítězství", "Liberation Day", countryCode));
             items.Add(new PublicHoliday(year, 7, 5, "Den slovanských věrozvěstů Cyrila a Metoděje", "Saints Cyril and Methodius Day", countryCode));
             items.Add(new PublicHoliday(year, 7, 6, "Den upálení mistra Jana Husa", "Jan Hus Day", countryCode));
             items.Add(new PublicHoliday(year, 9, 28, "Den české státnosti", "St. Wenceslas Day", countryCode));
             items.Add(new PublicHoliday(year, 10, 28, "Den vzniku samostatného československého státu", "Independent Czechoslovak State Day", countryCode));
-            items.Add(new PublicHoliday(year, 11, 17, "Den boje za svobodu a demokracii", "Struggle for Freedom and Democracy Day", countryCode));
+            items.Add(new PublicHoliday(year, 11, 17, "Den boje za svobodu a demokracii a Mezinárodní den studentstva", "Struggle for Freedom and Democracy Day", countryCode));
             items.Add(new PublicHoliday(year, 12, 24, "Štědrý den", "Christmas Eve", countryCode));
             items.Add(new PublicHoliday(year, 12, 25, "1. svátek vánoční", "Christmas Day", countryCode));
             items.Add(new PublicHoliday(year, 12, 26, "2. svátek vánoční", "St. Stephen's Day", countryCode));
--- Src/Nager.Date/PublicHolidays/SlovakiaProvider.cs
+++ Src/Nager.Date/PublicHolidays/SlovakiaProvider.cs
@@ -38,11 +38,11 @@ namespace Nager.Date.PublicHolidays
             items.Add(new PublicHoliday(easterSunday.AddDays(1), "Veľkonočný pondelok", "Easter Monday", countryCode));
             items.Add(new PublicHoliday(year, 5, 1, "Sviatok práce", "International Workers' Day", countryCode));
             items.Add(new PublicHoliday(year, 5, 8, "Deň víťazstva nad fašizmom", "Day of victory over fascism", countryCode));
-            items.Add(new PublicHoliday(year, 7, 5, "Sviatok svätého Cyrila a Metoda", "St. Cyril and Methodius Day", countryCode));
+            items.Add(new PublicHoliday(year, 7, 5, "Sviatok svätého Cyrila a svätého Metoda", "St. Cyril and Methodius Day", countryCode));
             items.Add(new PublicHoliday(year, 8, 29, "Výročie Slovenského národného povstania", "Slovak National Uprising anniversary", countryCode));
             items.Add(new PublicHoliday(year, 9, 1, "Deň Ústavy Slovenskej republiky", "Day of the Constitution of the Slovak Republic", countryCode));
-            items.Add(new PublicHoliday(year, 9, 15, "Sviatok Panny Márie Sedembolestnej", "Day of Our Lady of the Seven Sorrows", countryCode));
-            items.Add(new PublicHoliday(year, 11, 1, "Sviatok všetkých svätých", "All Saints’ Day", countryCode));
+            items.Add(new PublicHoliday(year, 9, 15, "Sedembolestná Panna Mária", "Day of Our Lady of the Seven Sorrows", countryCode));
+            items.Add(new PublicHoliday(year, 11, 1, "Sviatok Všetkých svätých", "All Saints’ Day", countryCode));
             items.Add(new PublicHoliday(year, 11, 17, "Deň boja za slobodu a demokraciu", "Struggle for Freedom and Democracy Day", countryCode));
             items.Add(new PublicHoliday(year, 12, 24, "Štedrý deň", "Christmas Eve", countryCode));
             items.Add(new PublicHoliday(year, 12, 25, "Prvý sviatok vianočný", "Christmas Day", countryCode));
