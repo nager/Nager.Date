66f390198a594de8201408cdb708843e644bcb48
Tino Hager <tino.hager@nager.at>
2020-01-07 23:26:20 +0100

    Optimize Long Weekend calculation

--- Src/Nager.Date.UnitTest/Common/LongWeekendTest.cs
+++ Src/Nager.Date.UnitTest/Common/LongWeekendTest.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nager.Date.Contract;
+using Nager.Date.Model;
+using Nager.Date.Weekends;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Nager.Date.UnitTest.Common
@@ -7,11 +11,27 @@ namespace Nager.Date.UnitTest.Common
     public class LongWeekendTest
     {
         [TestMethod]
-        public void LongWeekend()
+        public void LongWeekend1()
         {
             var longWeekends = DateSystem.GetLongWeekend(2017, CountryCode.AT).ToArray();
 
             Assert.AreEqual(10, longWeekends.Length);
+        }
+
+        [TestMethod]
+        public void LongWeekend2()
+        {
+            var publicHolidays = new PublicHoliday[]
+            {
+                new PublicHoliday(2020, 01, 10, "Mock1", "Mock1", CountryCode.AT),
+                new PublicHoliday(2020, 01, 13, "Mock2", "Mock2", CountryCode.AT),
+            };
+
+            ILongWeekendCalculator longWeekendCalculator = new LongWeekendCalculator(WeekendProvider.Universal);
+            var longWeekends = longWeekendCalculator.Calculate(publicHolidays);
+
+            Assert.AreEqual(1, longWeekends.Count());
+            Assert.AreEqual(4, longWeekends.First().DayCount);
         }
     }
 }
---
+++ Src/Nager.Date/Contract/LongWeekendCalculator.cs
@@ -0,0 +1,87 @@
+﻿using Nager.Date.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nager.Date.Contract
+{
+    /// <summary>
+    /// LongWeekendCalculator
+    /// </summary>
+    public class LongWeekendCalculator : ILongWeekendCalculator
+    {
+        private readonly IWeekendProvider _weekendProvider;
+        private readonly IEnumerable<PublicHoliday> _publicHolidays;
+
+        /// <summary>
+        /// LongWeekendCalculator
+        /// </summary>
+        /// <param name="weekendProvider"></param>
+        public LongWeekendCalculator(IWeekendProvider weekendProvider)
+        {
+            this._weekendProvider = weekendProvider;
+        }
+
+        /// <summary>
+        /// Calculate long weekends
+        /// </summary>
+        /// <param name="publicHolidays"></param>
+        /// <returns></returns>
+        public IEnumerable<LongWeekend> Calculate(IEnumerable<PublicHoliday> publicHolidays)
+        {
+            var items = new List<LongWeekend>();
+
+            foreach (var publicHoliday in publicHolidays)
+            {
+                var previoudDayCount = this.AvailableDays(publicHoliday.Date, -1, publicHolidays);
+                var nextDayCount = this.AvailableDays(publicHoliday.Date, 1, publicHolidays);
+
+                if (previoudDayCount == 0 && nextDayCount == 0)
+                {
+                    continue;
+                }
+
+                var startDate = publicHoliday.Date.AddDays(-previoudDayCount);
+                var endDate = publicHoliday.Date.AddDays(nextDayCount);
+
+                if (items.Any(o => o.StartDate.Equals(startDate) && o.EndDate.Equals(endDate)))
+                {
+                    continue;
+                }
+
+                items.Add(new LongWeekend
+                {
+                    Bridge = false,
+                    StartDate = startDate,
+                    EndDate = endDate
+                });
+            }
+
+            return items;
+        }
+
+        private int AvailableDays(DateTime startDate, int addValue, IEnumerable<PublicHoliday> publicHolidays)
+        {
+            var calculationDate = startDate.AddDays(addValue);
+            var count = 0;
+
+            while (this.ContinueWith(calculationDate, publicHolidays))
+            {
+                calculationDate = calculationDate.AddDays(addValue);
+                count++;
+            }
+
+            return count;
+        }
+
+        private bool ContinueWith(DateTime givenDate, IEnumerable<PublicHoliday> publicHolidays)
+        {
+            if (this._weekendProvider.WeekendDays.Contains(givenDate.DayOfWeek))
+            {
+                return true;
+            }
+
+            return publicHolidays.Any(o => o.Date.Date.Equals(givenDate.Date));
+        }
+    }
+}
