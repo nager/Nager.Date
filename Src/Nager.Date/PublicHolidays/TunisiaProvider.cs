a88cc7e42927128d3fbf7bebc4df146871a742bb
Tino Hager <tino.hager@nager.at>
2021-01-21 21:23:39 +0100

    Optimize summary

--- Src/Nager.Date/PublicHolidays/TunisiaProvider.cs
+++ Src/Nager.Date/PublicHolidays/TunisiaProvider.cs
@@ -17,11 +17,7 @@ namespace Nager.Date.PublicHolidays
         {
         }
 
-        /// <summary>
-        /// Get
-        /// </summary>
-        /// <param name="year">The year</param>
-        /// <returns></returns>
+        ///<inheritdoc/>
         public IEnumerable<PublicHoliday> Get(int year)
         {
             //TODO:Add moon calendar logic
@@ -46,10 +42,7 @@ namespace Nager.Date.PublicHolidays
             return items.OrderBy(o => o.Date);
         }
 
-        /// <summary>
-        /// Get the Holiday Sources
-        /// </summary>
-        /// <returns></returns>
+        ///<inheritdoc/>
         public IEnumerable<string> GetSources()
         {
             return new string[]
