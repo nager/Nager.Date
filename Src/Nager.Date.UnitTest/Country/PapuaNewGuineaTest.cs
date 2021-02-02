f1e8c3ffc7e1e480039283a966f983e6cbaa1e63
Tino Hager <tino.hager@nager.at>
2021-02-03 00:08:22 +0100

    Fix test

--- Src/Nager.Date.UnitTest/Country/PapuaNewGuineaTest.cs
+++ Src/Nager.Date.UnitTest/Country/PapuaNewGuineaTest.cs
@@ -22,7 +22,7 @@ namespace Nager.Date.UnitTest.Country
             var publicHolidays = DateSystem.GetPublicHoliday(2021, CountryCode.PG).ToArray();
 
             var publicHoliday = publicHolidays.FirstOrDefault(holiday => holiday.Name.Equals("Queen's Birthday"));
-            Assert.AreEqual(new DateTime(2018, 6, 11), publicHoliday.Date);
+            Assert.AreEqual(new DateTime(2021, 6, 14), publicHoliday.Date);
         }
     }
 }
