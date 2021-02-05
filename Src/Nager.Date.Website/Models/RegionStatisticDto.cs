ae7199ccf8ac58ed71f2900fa17d507ac17255c8
Tino Hager <tino.hager@nager.at>
2021-02-05 22:32:41 +0100

    Add region coverage info

--- Src/Nager.Date.Website/Models/RegionStatisticDto.cs
+++ Src/Nager.Date.Website/Models/RegionStatisticDto.cs
@@ -1,9 +1,30 @@
-﻿namespace Nager.Date.Website.Models
+﻿using System;
+
+namespace Nager.Date.Website.Models
 {
     public class RegionStatisticDto
     {
         public string RegionName { get; set; }
         public CountryInfoDto[] AvailableCountries { get; set; }
         public CountryInfoDto[] MissingCountries { get; set; }
+        public double Coverage
+        {
+            get
+            {
+                if (this.MissingCountries == null)
+                {
+                    return 0;
+                }
+
+                if (this.AvailableCountries == null)
+                {
+                    return 0;
+                }
+
+                var total = this.AvailableCountries.Length + this.MissingCountries.Length;
+
+                return Math.Round(100 - 100.0 * this.MissingCountries.Length / total, 2);
+            }
+        }
     }
 }
