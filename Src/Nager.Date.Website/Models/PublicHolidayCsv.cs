396e67529d1f801779a0f8795fdfaca00d55fe37
Tino Hager <tino.hager@nager.at>
2021-02-09 21:18:00 +0100

    Optimize summary

--- Src/Nager.Date.Website/Models/PublicHolidayCsv.cs
+++ Src/Nager.Date.Website/Models/PublicHolidayCsv.cs
@@ -2,26 +2,59 @@
 
 namespace Nager.Date.Website.Models
 {
+    /// <summary>
+    /// PublicHoliday Csv
+    /// </summary>
     public class PublicHolidayCsv
     {
+        /// <summary>
+        /// Date
+        /// </summary>
         public string Date { get; }
+        /// <summary>
+        /// LocalName
+        /// </summary>
         public string LocalName { get; }
+        /// <summary>
+        /// Name
+        /// </summary>
         public string Name { get; }
-        //ISO 3166-1 alpha-2
+        /// <summary>
+        /// CountryCode (ISO 3166-1 alpha-2)
+        /// </summary>
         public string CountryCode { get; }
+        /// <summary>
+        /// Fixed
+        /// </summary>
         public bool Fixed { get; }
+        /// <summary>
+        /// Global
+        /// </summary>
         public bool Global { get; }
+        /// <summary>
+        /// LaunchYear
+        /// </summary>
         public int? LaunchYear { get; }
+        /// <summary>
+        /// Type
+        /// </summary>
         public PublicHolidayType Type { get; }
         /// <summary>
         /// Counties in ISO_3166-2
         /// </summary>
         public string Counties { get; }
 
+        /// <summary>
+        /// PublicHoliday Csv
+        /// </summary>
         public PublicHolidayCsv()
         {
         }
 
+        /// <summary>
+        /// PublicHoliday Csv
+        /// </summary>
+        /// <param name="item"></param>
         public PublicHolidayCsv(PublicHoliday item)
         {
             this.Date = item.Date.ToString("yyyy-MM-dd");
