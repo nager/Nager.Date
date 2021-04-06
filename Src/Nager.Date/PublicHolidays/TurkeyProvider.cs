376f2aee3bf6561a28a5e42a5cdb5bf249dd5fbd
Ali Bekdaş <ali.bekdas@kartega.com>
2021-04-06 17:02:03 +0300

    Add Feast of Ramadan & Feast of Eid & Republic Day Eve for #255

--- Src/Nager.Date/PublicHolidays/TurkeyProvider.cs
+++ Src/Nager.Date/PublicHolidays/TurkeyProvider.cs
@@ -1,6 +1,8 @@
 ﻿using Nager.Date.Contract;
 using Nager.Date.Model;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace Nager.Date.PublicHolidays
@@ -29,7 +31,43 @@ namespace Nager.Date.PublicHolidays
             items.Add(new PublicHoliday(year, 5, 19, "Atatürk'ü Anma, Gençlik ve Spor Bayramı", "Atatürk Commemoration & Youth Day", countryCode));
             items.Add(new PublicHoliday(year, 7, 15, "Demokrasi Bayramı", "Democracy Day", countryCode));
             items.Add(new PublicHoliday(year, 8, 30, "Zafer Bayramı", "Victory Day", countryCode));
+            items.Add(new PublicHoliday(year, 10, 28, "Cumhuriyet Bayramı Arifesi", "Republic Day Eve", countryCode));
             items.Add(new PublicHoliday(year, 10, 29, "Cumhuriyet Bayramı", "Republic Day", countryCode));
+
+
+            HijriCalendar hijri = new HijriCalendar();
+            CultureInfo arSA = new CultureInfo("ar-SA");
+
+            var nowHijriYear = hijri.GetYear(new DateTime(year, 1, 1));
+
+            //Add Feast of Ramadan
+            var gregorianCalendarRamadanEve = DateTime.ParseExact($"30/09/{nowHijriYear}", "dd/MM/yyyy", arSA);
+            items.Add(new PublicHoliday(gregorianCalendarRamadanEve.Year, gregorianCalendarRamadanEve.Month, gregorianCalendarRamadanEve.Day, "Ramazan Bayramı Arifesi", "Ramadan Holiday Eve", countryCode));
+
+            var gregorianCalendarRamadan1 = DateTime.ParseExact($"01/10/{nowHijriYear}", "dd/MM/yyyy", arSA);
+            items.Add(new PublicHoliday(gregorianCalendarRamadan1.Year, gregorianCalendarRamadan1.Month, gregorianCalendarRamadan1.Day, "Ramazan Bayramı 1. Günü", "Ramadan Holiday First Day", countryCode));
+
+            var gregorianCalendarRamadan2 = DateTime.ParseExact($"02/10/{nowHijriYear}", "dd/MM/yyyy", arSA);
+            items.Add(new PublicHoliday(gregorianCalendarRamadan2.Year, gregorianCalendarRamadan2.Month, gregorianCalendarRamadan2.Day, "Ramazan Bayramı 2. Günü", "Ramadan Holiday Second Day", countryCode));
+
+            var gregorianCalendarRamadan3 = DateTime.ParseExact($"03/10/{nowHijriYear}", "dd/MM/yyyy", arSA);
+            items.Add(new PublicHoliday(gregorianCalendarRamadan3.Year, gregorianCalendarRamadan3.Month, gregorianCalendarRamadan3.Day, "Ramazan Bayramı 3. Günü", "Ramadan Holiday Third Day", countryCode));
+
+            //Add Feast of Eid
+            var gregorianCalendarSacrificeArife = DateTime.ParseExact($"09/12/{nowHijriYear}", "dd/MM/yyyy", arSA);
+            items.Add(new PublicHoliday(gregorianCalendarSacrificeArife.Year, gregorianCalendarSacrificeArife.Month, gregorianCalendarSacrificeArife.Day, "Kurban Bayramı Arifesi", "Eid Holiday Eve", countryCode));
+
+            var gregorianCalendarSacrifice1 = DateTime.ParseExact($"10/12/{nowHijriYear}", "dd/MM/yyyy", arSA);
+            items.Add(new PublicHoliday(gregorianCalendarSacrifice1.Year, gregorianCalendarSacrifice1.Month, gregorianCalendarSacrifice1.Day, "Kurban Bayramı 1. Günü", "Eid Holiday First Day", countryCode));
+
+            var gregorianCalendarSacrifice2 = DateTime.ParseExact($"11/12/{nowHijriYear}", "dd/MM/yyyy", arSA);
+            items.Add(new PublicHoliday(gregorianCalendarSacrifice2.Year, gregorianCalendarSacrifice2.Month, gregorianCalendarSacrifice2.Day, "Kurban Bayramı 2. Günü", "Eid Holiday Second Day", countryCode));
+
+            var gregorianCalendarSacrifice3 = DateTime.ParseExact($"12/12/{nowHijriYear}", "dd/MM/yyyy", arSA);
+            items.Add(new PublicHoliday(gregorianCalendarSacrifice3.Year, gregorianCalendarSacrifice3.Month, gregorianCalendarSacrifice3.Day, "Kurban Bayramı 3. Günü", "Eid Holiday Third Day", countryCode));
+
+            var gregorianCalendarSacrifice4 = DateTime.ParseExact($"13/12/{nowHijriYear}", "dd/MM/yyyy", arSA);
+            items.Add(new PublicHoliday(gregorianCalendarSacrifice4.Year, gregorianCalendarSacrifice4.Month, gregorianCalendarSacrifice4.Day, "Kurban Bayramı 4. Günü", "Eid Holiday Fourth Day", countryCode));
 
             return items.OrderBy(o => o.Date);
         }
