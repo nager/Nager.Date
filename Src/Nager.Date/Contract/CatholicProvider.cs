6281b04975024fcf85b7d1f6526a10a029c19b42
Martin Stühmer <me@samtrion.net>
2021-04-23 11:21:20 +0200

    Added simple thread-safe cache for the calculation of Easter Sunday. This means that this day is not recalculated regularly for recurring queries.

--- Src/Nager.Date/Contract/CatholicProvider.cs
+++ Src/Nager.Date/Contract/CatholicProvider.cs
@@ -1,32 +1,39 @@
-﻿using System;
+using System;
+using System.Collections.Concurrent;
 
 namespace Nager.Date.Contract
 {
+
     /// <summary>
     /// Catholic
     /// </summary>
     public class CatholicProvider : ICatholicProvider
     {
-        ///<inheritdoc/>
+        private static readonly ConcurrentDictionary<int, DateTime> _cache = new ConcurrentDictionary<int, DateTime>();
+
+        /// <inheritdoc/>
         public DateTime EasterSunday(int year)
         {
-            //http://stackoverflow.com/questions/2510383/how-can-i-calculate-what-date-good-friday-falls-on-given-a-year
-
-            var g = year % 19;
-            var c = year / 100;
-            var h = (c - c / 4 - (8 * c + 13) / 25 + 19 * g + 15) % 30;
-            var i = h - (h / 28) * (1 - (h / 28) * (29 / (h + 1)) * ((21 - g) / 11));
-
-            var day = i - ((year + (int)(year / 4) + i + 2 - c + (int)(c / 4)) % 7) + 28;
-            var month = 3;
-
-            if (day > 31)
+            return _cache.GetOrAdd(year, y =>
             {
-                month++;
-                day -= 31;
-            }
+                //http://stackoverflow.com/questions/2510383/how-can-i-calculate-what-date-good-friday-falls-on-given-a-year
 
-            return new DateTime(year, month, day);
+                var g = y % 19;
+                var c = y / 100;
+                var h = (c - c / 4 - (8 * c + 13) / 25 + 19 * g + 15) % 30;
+                var i = h - (h / 28) * (1 - (h / 28) * (29 / (h + 1)) * ((21 - g) / 11));
+
+                var day = i - ((y + (int)(y / 4) + i + 2 - c + (int)(c / 4)) % 7) + 28;
+                var month = 3;
+
+                if (day > 31)
+                {
+                    month++;
+                    day -= 31;
+                }
+
+                return new DateTime(y, month, day);
+            });
         }
 
         ///<inheritdoc/>
