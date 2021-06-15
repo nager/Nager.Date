79eeff0834fcdbfc042bc030ed6d6cfd0d9f7c88
Pablo Araya <61434099+parayacr@users.noreply.github.com>
2021-06-14 23:54:58 -0600

    Fix Costa Rica public holidays (#276)
    * Fix Costa Rica holidays, add local name
    
    * Fix review comments
    
    * Add all exceptions years to avoid extra calls
    
    * Check exception years first
    
    * Review comments

--- Src/Nager.Date/PublicHolidays/CostaRicaProvider.cs
+++ Src/Nager.Date/PublicHolidays/CostaRicaProvider.cs
@@ -1,5 +1,7 @@
 using Nager.Date.Contract;
+using Nager.Date.Extensions;
 using Nager.Date.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -27,17 +29,26 @@ namespace Nager.Date.PublicHolidays
             var countryCode = CountryCode.CR;
 
             var items = new List<PublicHoliday>();
-            items.Add(new PublicHoliday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
-            items.Add(new PublicHoliday(year, 4, 11, "Juan Santa maria Day", "Juan Santa maria Day", countryCode));
-            items.Add(this._catholicProvider.MaundyThursday("Good Thursday", year, countryCode));
-            items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
-            items.Add(new PublicHoliday(year, 5, 1, "Labor Day", "Labor Day", countryCode));
-            items.Add(new PublicHoliday(year, 7, 25, "Guanacaste Day", "Guanacaste Day", countryCode));
-            items.Add(new PublicHoliday(year, 8, 2, "Virgin of Los Angeles Day", "Virgin of Los Angeles Day", countryCode));
-            items.Add(new PublicHoliday(year, 8, 15, "Mother´s Day", "Mother´s Day", countryCode));
-            items.Add(new PublicHoliday(year, 9, 15, "Independence Day", "Independence Day", countryCode));
-            items.Add(new PublicHoliday(year, 10, 12, "Cultures National Day", "Cultures National Day", countryCode));
-            items.Add(new PublicHoliday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
+            items.Add(new PublicHoliday(year, 1, 1, "Año Nuevo", "New Year's Day", countryCode));
+            items.Add(this._catholicProvider.MaundyThursday("Jueves Santo", year, countryCode));
+            items.Add(this._catholicProvider.GoodFriday("Viernes Santo", year, countryCode));
+            items.Add(new PublicHoliday(year, 8, 2, "Fiesta de Nuestra Señora de los Ángeles", "Feast of Our Lady of the Angels", countryCode));
+            items.Add(new PublicHoliday(year, 12, 25, "Navidad", "Christmas Day", countryCode));
+            items.Add(GetJuanSantamariaDay(year, countryCode));
+            items.Add(GetLabourDay(year, countryCode));
+            items.Add(GetAnnexationDay(year, countryCode));            
+            items.Add(GetMothersDay(year, countryCode));
+            items.Add(GetIndenpendenceDay(year, countryCode));            
+
+            //Law 9803
+            if (year >= 2020)
+            {
+                items.Add(GetArmyAbolitionDay(year, countryCode));
+            }
+            else
+            {
+                items.Add(new PublicHoliday(year, 10, 12, "Día de las Culturas", "Cultures Day", countryCode));
+            }
 
             return items.OrderBy(o => o.Date);
         }
@@ -49,6 +60,83 @@ namespace Nager.Date.PublicHolidays
             {
                 "https://en.wikipedia.org/wiki/Public_holidays_in_Costa_Rica",
             };
+        }
+
+        private PublicHoliday GetJuanSantamariaDay(int year, CountryCode countryCode)
+        {
+            var juanSantamariaDay = new DateTime(year, 4, 11);
+            if (year == 2023 || year == 2024)
+            {
+                juanSantamariaDay = ApplyLaw9875Shift(juanSantamariaDay);
+            }
+
+            return new PublicHoliday(juanSantamariaDay, "Día de Juan Santamaría", "Juan Santamaría Day", countryCode);
+        }
+
+        private PublicHoliday GetLabourDay(int year, CountryCode countryCode)
+        {
+            var labourDay = new DateTime(year, 5, 1);
+            if (year == 2021)
+            {
+                labourDay = ApplyLaw9875Shift(labourDay);
+            }
+
+            return new PublicHoliday(labourDay, "Día Internacional del Trabajo", "Labour Day", countryCode);
+        }
+
+        private PublicHoliday GetAnnexationDay(int year, CountryCode countryCode)
+        {
+            var annexationDay = new DateTime(year, 7, 25);
+            if (year == 2020 || year == 2021 || year == 2023 || year == 2024)
+            {
+                annexationDay = ApplyLaw9875Shift(annexationDay);
+            }
+
+            return new PublicHoliday(annexationDay, "Anexión del Partido de Nicoya a Costa Rica", "Annexation of the Party of Nicoya to Costa Rica", countryCode);
+        }
+
+        private PublicHoliday GetMothersDay(int year, CountryCode countryCode)
+        {
+            var mothersDay = new DateTime(year, 8, 15);
+            if (year == 2020 || year == 2023 || year == 2024)
+            {
+                mothersDay = ApplyLaw9875Shift(mothersDay);
+            }
+
+            return new PublicHoliday(mothersDay, "Día de la Madre", "Mother's Day", countryCode);
+        }
+
+        private PublicHoliday GetIndenpendenceDay(int year, CountryCode countryCode)
+        {
+            var indenpendenceDay = new DateTime(year, 9, 15);
+            if (year == 2020 || year == 2021 || year == 2022)
+            {
+                indenpendenceDay = ApplyLaw9875Shift(indenpendenceDay);
+            }
+
+            return new PublicHoliday(indenpendenceDay, "Día de la Independencia", "Independence Day", countryCode);
+        }
+
+        private PublicHoliday GetArmyAbolitionDay(int year, CountryCode countryCode)
+        {
+            var armyAbolitionDay = new DateTime(year, 12, 1);
+            if (year == 2020 || year == 2021 || year == 2022)
+            {
+                armyAbolitionDay = ApplyLaw9875Shift(armyAbolitionDay);
+            }
+
+            return new PublicHoliday(armyAbolitionDay, "Día de la Abolición del Ejército", "Army Abolition Day", countryCode);
+        }
+
+        private DateTime ApplyLaw9875Shift(DateTime date)
+        {
+            date = date.Shift(saturday: saturday => saturday.AddDays(2), sunday: sunday => sunday.AddDays(1));
+            date = date.ShiftWeekdays(
+                tuesday: tuesday => tuesday.AddDays(-1),
+                wednesday: wednesday => wednesday.AddDays(-2),
+                thursday: thursday => thursday.AddDays(4)
+                );
+            return date;
         }
     }
 }
