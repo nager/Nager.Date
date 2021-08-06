1b0406a8c0db7dd473d02ef39c5ab7956cee34a6
Marcelo366 <44989613+Marcelo366@users.noreply.github.com>
2021-08-06 09:18:22 -0400

    some holidays are modified

--- Src/Nager.Date/PublicHolidays/ChileProvider.cs
+++ Src/Nager.Date/PublicHolidays/ChileProvider.cs
@@ -65,19 +65,20 @@ namespace Nager.Date.PublicHolidays
             items.Add(new PublicHoliday(year, 6, 26, "San Pedro y San Pablo", "Saint Peter and Saint Paul", countryCode));
             //Presidential and Congress Primary Elections
             items.Add(new PublicHoliday(year, 7, 16, "Virgen del Carmen", "Our Lady of Mount Carmel", countryCode));
-            items.Add(new PublicHoliday(year, 8, 10, "San Lorenzo de Tarapacá", "Saint Lawrence", countryCode, null, new string[] { "CL-TA" }));
-            items.Add(new PublicHoliday(year, 8, 10, "Día del Minero", "National Miner's Day", countryCode, null, new string[] { "CL-AT" }));
+            //items.Add(new PublicHoliday(year, 8, 10, "San Lorenzo de Tarapacá", "Saint Lawrence", countryCode, null, new string[] { "CL-TA" }));
+            //items.Add(new PublicHoliday(year, 8, 10, "Día del Minero", "National Miner's Day", countryCode, null, new string[] { "CL-AT" }));
             items.Add(new PublicHoliday(year, 8, 15, "Asunción de la Virgen", "Assumption of Mary", countryCode));
             //items.Add(new PublicHoliday(year, 8, 20, "Nacimiento del Prócer de la Independencia", "Nativity of Liberator Bernardo O'Higgins", countryCode)); //Only Valid in a communes
+            items.Add(new PublicHoliday(year, 9, 17, "Fiestas Patrias	", "National holiday", countryCode));
             items.Add(new PublicHoliday(year, 9, 18, "Fiestas Patrias", "National holiday", countryCode));
             items.Add(new PublicHoliday(year, 9, 19, "Día de las Glorias del Ejército", "Army Day", countryCode));
-            items.Add(new PublicHoliday(year, 9, 20, "Feast of La Pampilla", "Fiesta de La Pampilla", countryCode, null, new string[] { "CL-CO" }));
-            items.Add(new PublicHoliday(year, 9, 21, "Toma de posesión del estrecho de Magallanes", "National Possession of the Strait of Magellan", countryCode, null, new string[] { "CL-MA" }));
-            items.Add(new PublicHoliday(year, 10, 2, "Aniversario de la Creación de la XIV Región de Los Ríos", "Los Ríos Region Anniversary", countryCode, null, new string[] { "CL-LR" }));
-            items.Add(new PublicHoliday(year, 10, 9, "Día del Descubrimiento de Dos Mundos", "Columbus Day", countryCode));
-            items.Add(new PublicHoliday(year, 10, 27, "Día Nacional de las Iglesias Evangélicas y Protestantes", "Reformation Day", countryCode));
+            //items.Add(new PublicHoliday(year, 9, 20, "Feast of La Pampilla", "Fiesta de La Pampilla", countryCode, null, new string[] { "CL-CO" }));
+            //items.Add(new PublicHoliday(year, 9, 21, "Toma de posesión del estrecho de Magallanes", "National Possession of the Strait of Magellan", countryCode, null, new string[] { "CL-MA" }));
+            //items.Add(new PublicHoliday(year, 10, 2, "Aniversario de la Creación de la XIV Región de Los Ríos", "Los Ríos Region Anniversary", countryCode, null, new string[] { "CL-LR" }));
+            items.Add(new PublicHoliday(year, 10, 11, "Encuentro de Dos Mundos", "Columbus Day", countryCode));
+            items.Add(new PublicHoliday(year, 10, 31, "Día Nacional de las Iglesias Evangélicas y Protestantes", "Reformation Day", countryCode));
             items.Add(new PublicHoliday(year, 11, 1, "Día de Todos los Santos", "All Saints", countryCode));
-            items.Add(new PublicHoliday(year, 11, 19, "Elecciones presidencial, congresistas y regionales", "Presidential, parliamentary and regional elections", countryCode));
+            items.Add(new PublicHoliday(year, 11, 21, "Elecciones presidencial, congresistas y regionales", "Presidential, parliamentary and regional elections", countryCode));
             items.Add(new PublicHoliday(year, 12, 8, "Inmaculada Concepción", "Immaculate Conception", countryCode));
             //Presidential election, runoff
             items.Add(new PublicHoliday(year, 12, 25, "Navidad / Natividad del Señor", "Christmas Day", countryCode));
