fa7280917bea891c4f4f9b8fdf9ab3b01334d092
Ruan Yiping <yiping.ruan@outlook.com>
2021-08-15 17:50:52 +0800

    Add year 2020/2021

--- Src/Nager.Date/PublicHolidays/SingaporeProvider.cs
+++ Src/Nager.Date/PublicHolidays/SingaporeProvider.cs
@@ -1,5 +1,6 @@
 using Nager.Date.Contract;
 using Nager.Date.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -20,14 +21,19 @@ namespace Nager.Date.PublicHolidays
         ///<inheritdoc/>
         public IEnumerable<PublicHoliday> Get(int year)
         {
+            if (year < 2020 || year > 2022)
+            {
+                throw new NotImplementedException($"Year {year} for Singapore is not implemented.");
+            }
+
             var countryCode = CountryCode.SG;
 
+            // Fixed holidays
             var items = new List<PublicHoliday>
             {
                 new PublicHoliday(year, 1, 1, "New Year’s Day", "New Year’s Day", countryCode),
                 new PublicHoliday(year, 2, 1, "Chinese New Year", "Chinese New Year", countryCode),
                 new PublicHoliday(year, 2, 2, "Chinese New Year", "Chinese New Year", countryCode),
-                new PublicHoliday(year, 4, 15, "Good Friday", "Good Friday", countryCode),
                 new PublicHoliday(year, 5, 1, "Labour Day", "Labour Day", countryCode),
                 new PublicHoliday(year, 8, 9, "National Day", "National Day", countryCode),
                 new PublicHoliday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode),
@@ -35,7 +41,22 @@ namespace Nager.Date.PublicHolidays
 
             switch (year)
             {
+                case 2020:
+                    items.Add(new PublicHoliday(year, 4, 10, "Good Friday", "Good Friday", countryCode));
+                    items.Add(new PublicHoliday(year, 5, 24, "Hari Raya Puasa", "Hari Raya Puasa", countryCode));
+                    items.Add(new PublicHoliday(year, 5, 07, "Vesak Day", "Vesak Day", countryCode));
+                    items.Add(new PublicHoliday(year, 7, 31, "Hari Raya Haji", "Hari Raya Haji", countryCode));
+                    items.Add(new PublicHoliday(year, 11, 14, "Deepavali", "Deepavali", countryCode));
+                    break;
+                case 2021:
+                    items.Add(new PublicHoliday(year, 4, 2, "Good Friday", "Good Friday", countryCode));
+                    items.Add(new PublicHoliday(year, 5, 13, "Hari Raya Puasa", "Hari Raya Puasa", countryCode));
+                    items.Add(new PublicHoliday(year, 5, 26, "Vesak Day", "Vesak Day", countryCode));
+                    items.Add(new PublicHoliday(year, 7, 20, "Hari Raya Haji", "Hari Raya Haji", countryCode));
+                    items.Add(new PublicHoliday(year, 11, 4, "Deepavali", "Deepavali", countryCode));
+                    break;
                 case 2022:
+                    items.Add(new PublicHoliday(year, 4, 15, "Good Friday", "Good Friday", countryCode));
                     items.Add(new PublicHoliday(year, 5, 2, "Hari Raya Puasa", "Hari Raya Puasa", countryCode));
                     items.Add(new PublicHoliday(year, 5, 15, "Vesak Day", "Vesak Day", countryCode));
                     items.Add(new PublicHoliday(year, 7, 9, "Hari Raya Haji", "Hari Raya Haji", countryCode));
@@ -53,6 +74,8 @@ namespace Nager.Date.PublicHolidays
         {
             return new string[]
             {
+                "https://www.mom.gov.sg/employment-practices/public-holidays#Year-2020",
+                "https://www.mom.gov.sg/employment-practices/public-holidays#Year-2021",
                 "https://www.mom.gov.sg/employment-practices/public-holidays#Year-2022"
             };
         }
