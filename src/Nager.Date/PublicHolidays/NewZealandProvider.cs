07761c85b225fbc49a1328be9c5eb59913421f46
Brent McSharry <2456704+mcshaz@users.noreply.github.com>
2021-11-04 08:40:43 +1300

    remove NZ variable abbreviations

--- src/Nager.Date/PublicHolidays/NewZealandProvider.cs
+++ src/Nager.Date/PublicHolidays/NewZealandProvider.cs
@@ -28,9 +28,9 @@ namespace Nager.Date.PublicHolidays
         {
             var countryCode = CountryCode.NZ;
 
-            var queensBthday = DateSystem.FindDay(year, Month.June, DayOfWeek.Monday, Occurrence.First);
+            var queensBirthday = DateSystem.FindDay(year, Month.June, DayOfWeek.Monday, Occurrence.First);
             var labourDay = DateSystem.FindDay(year, Month.October, DayOfWeek.Monday, Occurrence.Fourth);
-            var easterMon = this._catholicProvider.EasterMonday("Easter Monday", year, countryCode);
+            var easterMonday = this._catholicProvider.EasterMonday("Easter Monday", year, countryCode);
 
             var items = new List<PublicHoliday>();
 
@@ -88,51 +88,51 @@ namespace Nager.Date.PublicHolidays
 
             #region Regional Anniversary Days
             // https://www.employment.govt.nz/leave-and-holidays/public-holidays/public-holidays-and-anniversary-dates/
-            var auckl = new DateTime(year, 1, 29).ShiftToClosest(DayOfWeek.Monday);
-            items.Add(new PublicHoliday(auckl, "Auckland/Northland Anniversary Day", "Auckland Anniversary Day", countryCode, counties: new[] { "NZ-AUK", "NZ-NTL", "NZ-MWT", "NZ-WKO", "NZ-GIS", "NZ-BOP", "NZ-HKB" }));
+            var aucklandDay = new DateTime(year, 1, 29).ShiftToClosest(DayOfWeek.Monday);
+            items.Add(new PublicHoliday(aucklandDay, "Auckland/Northland Anniversary Day", "Auckland Anniversary Day", countryCode, counties: new[] { "NZ-AUK", "NZ-NTL", "NZ-MWT", "NZ-WKO", "NZ-GIS", "NZ-BOP", "NZ-HKB" }));
 
-            var wellington = new DateTime(year, 1, 22).ShiftToClosest(DayOfWeek.Monday);
-            items.Add(new PublicHoliday(wellington, "Wellington Anniversary Day", "Wellington Anniversary Day", countryCode, counties: new[] { "NZ-WGN", "NZ-MWT" }));
+            var wellingtonDay = new DateTime(year, 1, 22).ShiftToClosest(DayOfWeek.Monday);
+            items.Add(new PublicHoliday(wellingtonDay, "Wellington Anniversary Day", "Wellington Anniversary Day", countryCode, counties: new[] { "NZ-WGN", "NZ-MWT" }));
 
-            var canterburySth = DateSystem.FindDay(year, Month.September, DayOfWeek.Monday, Occurrence.Fourth);
-            items.Add(new PublicHoliday(canterburySth, "Dominion Day", "Canterbury (South) Anniversary Day", countryCode, counties: new[] { "NZ-CAN" }));
+            var canterburySouthDay = DateSystem.FindDay(year, Month.September, DayOfWeek.Monday, Occurrence.Fourth);
+            items.Add(new PublicHoliday(canterburySouthDay, "Dominion Day", "Canterbury (South) Anniversary Day", countryCode, counties: new[] { "NZ-CAN" }));
 
-            var chatham = new DateTime(year, 11, 30).ShiftToClosest(DayOfWeek.Monday);
-            items.Add(new PublicHoliday(chatham, "Chatham Islands Anniversary Day", "Chatham Islands Anniversary Day", countryCode, counties: new[] { "NZ-CIT" }));
+            var chathamDay = new DateTime(year, 11, 30).ShiftToClosest(DayOfWeek.Monday);
+            items.Add(new PublicHoliday(chathamDay, "Chatham Islands Anniversary Day", "Chatham Islands Anniversary Day", countryCode, counties: new[] { "NZ-CIT" }));
 
-            var nelson = new DateTime(year, 2, 1).ShiftToClosest(DayOfWeek.Monday);
-            items.Add(new PublicHoliday(nelson, "Nelson Anniversary Day", "Nelson Anniversary Day", countryCode, counties: new[] { "NZ-NSN" }));
+            var nelsonDay = new DateTime(year, 2, 1).ShiftToClosest(DayOfWeek.Monday);
+            items.Add(new PublicHoliday(nelsonDay, "Nelson Anniversary Day", "Nelson Anniversary Day", countryCode, counties: new[] { "NZ-NSN" }));
 
-            var otago = new DateTime(year, 3, 23).ShiftToClosest(DayOfWeek.Monday);
-            items.Add(new PublicHoliday(otago, "Otago Anniversary Day", "Otago Anniversary Day", countryCode, counties: new[] { "NZ-OTA" }));
+            var otagoDay = new DateTime(year, 3, 23).ShiftToClosest(DayOfWeek.Monday);
+            items.Add(new PublicHoliday(otagoDay, "Otago Anniversary Day", "Otago Anniversary Day", countryCode, counties: new[] { "NZ-OTA" }));
 
-            var taranaki = DateSystem.FindDay(year, Month.March, DayOfWeek.Monday, Occurrence.Second);
-            items.Add(new PublicHoliday(taranaki, "Taranaki Anniversary Day", "Taranaki Anniversary Day", countryCode, counties: new[] { "NZ-TKI" }));
+            var taranakiDay = DateSystem.FindDay(year, Month.March, DayOfWeek.Monday, Occurrence.Second);
+            items.Add(new PublicHoliday(taranakiDay, "Taranaki Anniversary Day", "Taranaki Anniversary Day", countryCode, counties: new[] { "NZ-TKI" }));
 
             // Fri before labour day, and labour day always a Mon
-            var hawkesBay = labourDay.AddDays(-3);
-            items.Add(new PublicHoliday(hawkesBay, "Hawke's Bay Anniversary Day", "Hawke's Bay Anniversary Day", countryCode, counties: new[] { "NZ-HKB" }));
+            var hawkesBayDay = labourDay.AddDays(-3);
+            items.Add(new PublicHoliday(hawkesBayDay, "Hawke's Bay Anniversary Day", "Hawke's Bay Anniversary Day", countryCode, counties: new[] { "NZ-HKB" }));
 
             // Mon following labour day (which is always Mon itself)
-            var marlborough = labourDay.AddDays(7);
-            items.Add(new PublicHoliday(marlborough, "Marlborough Anniversary Day", "Marlborough Anniversary Day", countryCode, counties: new[] { "NZ-MBH" }));
+            var marlboroughDay = labourDay.AddDays(7);
+            items.Add(new PublicHoliday(marlboroughDay, "Marlborough Anniversary Day", "Marlborough Anniversary Day", countryCode, counties: new[] { "NZ-MBH" }));
 
             // Easter Tues
-            var southland = easterMon.Date.AddDays(1);
-            items.Add(new PublicHoliday(southland, "Southland Anniversary Day", "Southland Anniversary Day", countryCode, counties: new[] { "NZ-STL" }));
+            var southlandDay = easterMonday.Date.AddDays(1);
+            items.Add(new PublicHoliday(southlandDay, "Southland Anniversary Day", "Southland Anniversary Day", countryCode, counties: new[] { "NZ-STL" }));
 
-            var westland = new DateTime(year, 12, 1).ShiftToClosest(DayOfWeek.Monday);
-            items.Add(new PublicHoliday(westland, "Westland Anniversary Day", "Westland Anniversary Day", countryCode, counties: new[] { "NZ-WTC" }));
+            var westlandDay = new DateTime(year, 12, 1).ShiftToClosest(DayOfWeek.Monday);
+            items.Add(new PublicHoliday(westlandDay, "Westland Anniversary Day", "Westland Anniversary Day", countryCode, counties: new[] { "NZ-WTC" }));
 
             // 2nd Fri following 1st Tues of Nov!
-            var canterbury = DateSystem.FindDay(year, Month.November, DayOfWeek.Tuesday, Occurrence.First).AddDays(10);
-            items.Add(new PublicHoliday(canterbury, "Canterbury (North & Central) Anniversary Day", "Canterbury Anniversary Day", countryCode, counties: new[] { "NZ-CAN" }));
+            var canterburyDay = DateSystem.FindDay(year, Month.November, DayOfWeek.Tuesday, Occurrence.First).AddDays(10);
+            items.Add(new PublicHoliday(canterburyDay, "Canterbury (North & Central) Anniversary Day", "Canterbury Anniversary Day", countryCode, counties: new[] { "NZ-CAN" }));
 
             #endregion
 
             items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
-            items.Add(easterMon);
-            items.Add(new PublicHoliday(queensBthday, "Queen's Birthday", "Queen's Birthday", countryCode));
+            items.Add(easterMonday);
+            items.Add(new PublicHoliday(queensBirthday, "Queen's Birthday", "Queen's Birthday", countryCode));
             items.Add(new PublicHoliday(labourDay, "Labour Day", "Labour Day", countryCode));
 
             return items.OrderBy(o => o.Date);
