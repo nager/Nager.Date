0058cc96fe1c21891356659a74fbc8d9b831a6d1
Emmanuel Bruno <e.bruno@askelia.com>
2022-08-23 18:35:11 +0200

    Added Shift rules to Albania According to wikipedia: "If a non-working public holiday happens during the weekend, then the Monday after would be non-working" However it is unclear what happens when january 1st is on saturday.  Depending on sources only monday is holiday,  while some others add the tuesday...

--- src/Nager.Date/PublicHolidays/AlbaniaProvider.cs
+++ src/Nager.Date/PublicHolidays/AlbaniaProvider.cs
@@ -29,10 +29,10 @@ namespace Nager.Date.PublicHolidays
             var countryCode = CountryCode.AL;
 
             var items = new List<PublicHoliday>();
-            items.Add(new PublicHoliday(year, 1, 1, "Viti i Ri", "New Year's Day", countryCode));
-            items.Add(new PublicHoliday(year, 1, 2, "Viti i Ri", "New Year's Day", countryCode));
-            items.Add(new PublicHoliday(year, 3, 14, "Dita e Verës", "Summer Day", countryCode));
-            items.Add(new PublicHoliday(year, 3, 22, "Dita e Sulltan Nevruzit", "Nowruz", countryCode));
+            items.Add(this.ApplyShiftingRules(new PublicHoliday(year, 1, 1, "Viti i Ri", "New Year's Day", countryCode)));
+            items.Add(this.ApplyShiftingRules(new PublicHoliday(year, 1, 2, "Viti i Ri", "New Year's Day", countryCode)));
+            items.Add(this.ApplyShiftingRules(new PublicHoliday(year, 3, 14, "Dita e Verës", "Summer Day", countryCode)));
+            items.Add(this.ApplyShiftingRules(new PublicHoliday(year, 3, 22, "Dita e Sulltan Nevruzit", "Nowruz", countryCode)));
             //Catholic Easter and monday
             items.Add(this._catholicProvider.EasterSunday("Pashkët Katolike", year, countryCode));
             items.Add(this._catholicProvider.EasterMonday("Hënen e Pashkët Katolike", year, countryCode));
@@ -43,13 +43,19 @@ namespace Nager.Date.PublicHolidays
             //TODO: Eid ul-Fitr is not implemented
             //TODO: Eid ul-Adha is not implemented
 
-            items.Add(new PublicHoliday(year, 10, 19, "Dita e Nënë Terezës", "Mother Teresa Day", countryCode));
-            items.Add(new PublicHoliday(year, 11, 28, "Dita e Pavarësisë", "Independence Day", countryCode));
-            items.Add(new PublicHoliday(year, 11, 29, "Dita e Çlirimit", "Liberation Day", countryCode));
-            items.Add(new PublicHoliday(year, 12, 08, "Dita Kombëtare e Rinisë", "Youth Day", countryCode));
-            items.Add(new PublicHoliday(year, 12, 25, "Krishtlindjet", "Christmas Day", countryCode));
+            items.Add(this.ApplyShiftingRules(new PublicHoliday(year, 10, 19, "Dita e Nënë Terezës", "Mother Teresa Day", countryCode)));
+            items.Add(this.ApplyShiftingRules(new PublicHoliday(year, 11, 28, "Dita e Pavarësisë", "Independence Day", countryCode)));
+            items.Add(this.ApplyShiftingRules(new PublicHoliday(year, 11, 29, "Dita e Çlirimit", "Liberation Day", countryCode)));
+            items.Add(this.ApplyShiftingRules(new PublicHoliday(year, 12, 08, "Dita Kombëtare e Rinisë", "Youth Day", countryCode)));
+            items.Add(this.ApplyShiftingRules(new PublicHoliday(year, 12, 25, "Krishtlindjet", "Christmas Day", countryCode)));
 
             return items.OrderBy(o => o.Date);
+        }
+
+        private PublicHoliday ApplyShiftingRules(PublicHoliday holiday)
+        {
+            return holiday
+                .Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(1))
         }
 
         ///<inheritdoc/>
