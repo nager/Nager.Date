13e59f790795fa853a284572bb598d309fac7eb5
Emmanuel Bruno <e.bruno@askelia.com>
2022-08-24 10:37:37 +0200

    deleted extra space

--- src/Nager.Date/PublicHolidays/AndorraProvider.cs
+++ src/Nager.Date/PublicHolidays/AndorraProvider.cs
@@ -28,20 +28,20 @@ namespace Nager.Date.PublicHolidays
             var countryCode = CountryCode.AD;
 
             var items = new List<PublicHoliday>();
-            items.Add(new PublicHoliday(year, 1, 1, "Any nou", "New Year's Day", countryCode ));
-            items.Add(new PublicHoliday(year, 1, 6, "Reis", "Epiphany", countryCode ));
+            items.Add(new PublicHoliday(year, 1, 1, "Any nou", "New Year's Day", countryCode));
+            items.Add(new PublicHoliday(year, 1, 6, "Reis", "Epiphany", countryCode));
             ///TODO: Carnaval is not implemented : how to find the date ? 
-            items.Add(new PublicHoliday(year, 3, 14, "Dia de la Constitució", "Constitution Day", countryCode ));
+            items.Add(new PublicHoliday(year, 3, 14, "Dia de la Constitució", "Constitution Day", countryCode));
             items.Add(this._catholicProvider.GoodFriday("Divendres Sant", year, countryCode);
             items.Add(this._catholicProvider.EasterMonday("Dilluns de Pasqua", year, countryCode);        
-            items.Add(new PublicHoliday(year, 5, 1, "Festa del Treball", "Labour Day", countryCode ));
+            items.Add(new PublicHoliday(year, 5, 1, "Festa del Treball", "Labour Day", countryCode));
             items.Add(this._catholicProvider.WhitMonday("Dilluns de Pentecosta", year, countryCode);
-            items.Add(new PublicHoliday(year, 8, 15, "Assumpció", "Assumption Day", countryCode ));
-            items.Add(new PublicHoliday(year, 9, 8, "Mare de Déu de Meritxell", "National Holiday", countryCode ));
-            items.Add(new PublicHoliday(year, 11, 1, "Tots Sants", "All Saints' Day", countryCode ));
-            items.Add(new PublicHoliday(year, 12, 8, "Immaculada Concepció", "Immaculate Conception", countryCode ));
-            items.Add(new PublicHoliday(year, 12, 25, "Nadal", "Christmas Day", countryCode ));
-            items.Add(new PublicHoliday(year, 12, 26, "Sant Esteve", "Saint Stephen's Day", countryCode ));
+            items.Add(new PublicHoliday(year, 8, 15, "Assumpció", "Assumption Day", countryCode));
+            items.Add(new PublicHoliday(year, 9, 8, "Mare de Déu de Meritxell", "National Holiday", countryCode));
+            items.Add(new PublicHoliday(year, 11, 1, "Tots Sants", "All Saints' Day", countryCode));
+            items.Add(new PublicHoliday(year, 12, 8, "Immaculada Concepció", "Immaculate Conception", countryCode));
+            items.Add(new PublicHoliday(year, 12, 25, "Nadal", "Christmas Day", countryCode));
+            items.Add(new PublicHoliday(year, 12, 26, "Sant Esteve", "Saint Stephen's Day", countryCode));
 
 
             return items.OrderBy(o => o.Date);
