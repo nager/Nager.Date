5da35a43e8cd16977acd5cab81d3e75cc2c6d2cf
Tino Hager <tino.hager@nager.at>
2023-04-16 20:58:37 +0200

    Optimize code for mexico provider

--- src/Nager.Date/PublicHolidays/MexicoProvider.cs
+++ src/Nager.Date/PublicHolidays/MexicoProvider.cs
@@ -32,7 +32,6 @@ namespace Nager.Date.PublicHolidays
             var newYearDay = new DateTime(year, 1, 1).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
             var laborDay = new DateTime(year, 5, 1).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
             var independenceDay = new DateTime(year, 9, 16).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
-            var inaugurationDay = new DateTime(year, 12, 1).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
 
             var items = new List<PublicHoliday>();
             items.Add(new PublicHoliday(newYearDay, "Año Nuevo", "New Year's Day", countryCode));
@@ -41,13 +40,21 @@ namespace Nager.Date.PublicHolidays
             items.Add(new PublicHoliday(laborDay, "Día del Trabajo", "Labor Day", countryCode));
             items.Add(new PublicHoliday(independenceDay, "Día de la Independencia", "Independence Day", countryCode));
             items.Add(new PublicHoliday(thirdMondayOfNovember, "Día de la Revolución", "Revolution Day", countryCode));
-            if ((year - 2) % 6 == 0)
-            {
-                items.Add(new PublicHoliday(inaugurationDay, "Transmisión del Poder Ejecutivo Federal", "Inauguration Day", countryCode));
-            }
             items.Add(new PublicHoliday(year, 12, 25, "Navidad", "Christmas Day", countryCode));
+            items.AddIfNotNull(this.InaugurationDay(year, countryCode));
 
             return items.OrderBy(o => o.Date);
+        }
+
+        private PublicHoliday InaugurationDay(int year, CountryCode countryCode)
+        {
+            // Every 6 years on 1. December
+            if ((year - 2) % 6 == 0)
+            {
+                return new PublicHoliday(year, 12, 1, "Transmisión del Poder Ejecutivo Federal", "Inauguration Day", countryCode);
+            }
+
+            return null;
         }
 
         ///<inheritdoc/>
