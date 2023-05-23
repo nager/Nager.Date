af05d54166c7d9df9398e1da048a085fc7836507
Tino Hager <tino.hager@nager.at>
2023-05-23 21:12:56 +0200

    Germany - Fix World Childrens Day show before 2019 (#501)

--- src/Nager.Date/PublicHolidays/GermanyProvider.cs
+++ src/Nager.Date/PublicHolidays/GermanyProvider.cs
@@ -64,7 +64,6 @@ namespace Nager.Date.PublicHolidays
             items.Add(this._catholicProvider.WhitMonday("Pfingstmontag", year, countryCode));
             items.Add(this._catholicProvider.CorpusChristi("Fronleichnam", year, countryCode).SetCounties("DE-BW", "DE-BY", "DE-HE", "DE-NW", "DE-RP", "DE-SL"));
             items.Add(new PublicHoliday(year, 8, 15, "Mariä Himmelfahrt", "Assumption Day", countryCode, null, new string[] { "DE-SL" }));
-            items.Add(new PublicHoliday(year, 9, 20, "Weltkindertag", "World Children's Day", countryCode, 2019, new string[] { "DE-TH" }));
             items.Add(new PublicHoliday(year, 10, 3, "Tag der Deutschen Einheit", "German Unity Day", countryCode));
             items.Add(new PublicHoliday(year, 11, 1, "Allerheiligen", "All Saints' Day", countryCode, null, new string[] { "DE-BW", "DE-BY", "DE-NW", "DE-RP", "DE-SL" }));
             items.Add(new PublicHoliday(year, 12, 25, "Erster Weihnachtstag", "Christmas Day", countryCode));
@@ -74,8 +73,19 @@ namespace Nager.Date.PublicHolidays
             items.AddIfNotNull(this.PrayerDay(year, CountryCode.DE));
             items.AddIfNotNull(this.LiberationDay(year, CountryCode.DE));
             items.AddIfNotNull(this.ReformationDay(year, CountryCode.DE));
+            items.AddIfNotNull(this.WorldChildrensDay(year, CountryCode.DE));
 
             return items.OrderBy(o => o.Date);
+        }
+
+        private PublicHoliday WorldChildrensDay(int year, CountryCode countryCode)
+        {
+            if (year >= 2019)
+            {
+                return new PublicHoliday(year, 9, 20, "Weltkindertag", "World Children's Day", countryCode, 2019, new string[] { "DE-TH" });
+            }
+
+            return null;
         }
 
         private PublicHoliday InternationalWomensDay(int year, CountryCode countryCode)
