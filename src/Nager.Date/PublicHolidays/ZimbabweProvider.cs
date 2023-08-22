6fb0801d71234de7e7a7a0675d3710864ea63d06
Tino Hager <tino.hager@nager.at>
2023-08-22 21:24:05 +0200

    Update ZimbabweProvider.cs

--- src/Nager.Date/PublicHolidays/ZimbabweProvider.cs
+++ src/Nager.Date/PublicHolidays/ZimbabweProvider.cs
@@ -29,7 +29,7 @@ namespace Nager.Date.PublicHolidays
             var easterSunday = this._catholicProvider.EasterSunday(year);
 
             var secondMondayInAugust = DateSystem.FindDay(year, Month.August, DayOfWeek.Monday, Occurrence.Second);
-            var secondTuesdayInAugust = DateSystem.FindDay(year, Month.August, DayOfWeek.Tuesday, Occurrence.Second);
+            var tuesdayAfterSecondMondayInAugust = secondMondayInAugust.AddDays(1);
 
             var items = new List<PublicHoliday>();
             items.Add(new PublicHoliday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
@@ -41,7 +41,7 @@ namespace Nager.Date.PublicHolidays
             items.Add(new PublicHoliday(year, 5, 1, "Worker's Day", "Worker's Day", countryCode));
             items.Add(new PublicHoliday(year, 5, 25, "Africa Day", "Africa Day", countryCode));
             items.Add(new PublicHoliday(secondMondayInAugust, "Heroes' Day", "Heroes' Day", countryCode));
-            items.Add(new PublicHoliday(secondTuesdayInAugust, "Defence Forces Day", "Defence Forces Day", countryCode));
+            items.Add(new PublicHoliday(tuesdayAfterSecondMondayInAugust, "Defence Forces Day", "Defence Forces Day", countryCode));
             items.Add(new PublicHoliday(year, 12, 22, "Unity Day", "Unity Day", countryCode));
             items.Add(new PublicHoliday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
             items.Add(new PublicHoliday(year, 12, 26, "Boxing Day", "St. Stephen's Day", countryCode));
