3250ff8111b180a35b811835ce19c4d7ab286360
Tino Hager <tino.hager@nager.at>
2024-03-01 21:40:38 +0100

    migrate Guyana

--- src/Nager.Date/HolidayProviders/GuyanaHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/GuyanaHolidayProvider.cs
@@ -30,23 +30,95 @@ namespace Nager.Date.HolidayProviders
 
             var firstMondayInJuly = DateSystem.FindDay(year, Month.July, DayOfWeek.Monday, Occurrence.First);
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
-            items.Add(new Holiday(year, 2, 23, "Republic Day", "Republic Day", countryCode));
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 2, 23),
+                    EnglishName = "Republic Day",
+                    LocalName = "Republic Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Labour Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 5),
+                    EnglishName = "Arrival Day",
+                    LocalName = "Arrival Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 26),
+                    EnglishName = "Independence Day",
+                    LocalName = "Independence Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = firstMondayInJuly,
+                    EnglishName = "Caricom Day",
+                    LocalName = "Caricom Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 1),
+                    EnglishName = "Emancipation Day",
+                    LocalName = "Emancipation Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 26),
+                    EnglishName = "Boxing Day",
+                    LocalName = "Boxing Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.GoodFriday("Good Friday", year),
+                this._catholicProvider.EasterMonday("Easter Monday", year)
+            };
+
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
+            //items.Add(new Holiday(year, 2, 23, "Republic Day", "Republic Day", countryCode));
             //TODO:Phagwah ??? (Hindu calendar)
-            items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
-            items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
-            items.Add(new Holiday(year, 5, 1, "Labour Day", "Labour Day", countryCode));
-            items.Add(new Holiday(year, 5, 5, "Arrival Day", "Arrival Day", countryCode));
-            items.Add(new Holiday(year, 5, 26, "Independence Day", "Independence Day", countryCode));
-            items.Add(new Holiday(firstMondayInJuly, "Caricom Day", "Caricom Day", countryCode));
-            items.Add(new Holiday(year, 8, 1, "Emancipation Day", "Emancipation Day", countryCode));
+            //items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
+            //items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
+            //items.Add(new Holiday(year, 5, 1, "Labour Day", "Labour Day", countryCode));
+            //items.Add(new Holiday(year, 5, 5, "Arrival Day", "Arrival Day", countryCode));
+            //items.Add(new Holiday(year, 5, 26, "Independence Day", "Independence Day", countryCode));
+            //items.Add(new Holiday(firstMondayInJuly, "Caricom Day", "Caricom Day", countryCode));
+            //items.Add(new Holiday(year, 8, 1, "Emancipation Day", "Emancipation Day", countryCode));
             //TODO:Deepavali ??? (Hindu calendar)
-            items.Add(new Holiday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
-            items.Add(new Holiday(year, 12, 26, "Boxing Day", "Boxing Day", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
+            //items.Add(new Holiday(year, 12, 26, "Boxing Day", "Boxing Day", countryCode));
             //TODO:Youman-Nabi ??? (Muslim holidays)
             //TODO:Eid-ul-Adha ??? (Muslim holidays)
-            return items.OrderBy(o => o.Date);
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
