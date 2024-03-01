229a156bebafe96c103c0d144eb4adf6460c3eb9
Tino Hager <tino.hager@nager.at>
2024-03-01 21:02:46 +0100

    migrate France

--- src/Nager.Date/HolidayProviders/FranceHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/FranceHolidayProvider.cs
@@ -1,5 +1,6 @@
 using Nager.Date.Models;
 using Nager.Date.ReligiousProviders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -48,20 +49,86 @@ namespace Nager.Date.HolidayProviders
         {
             var countryCode = CountryCode.FR;
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "Jour de l'an", "New Year's Day", countryCode, 1967));
-            items.Add(this._catholicProvider.EasterMonday("Lundi de Pâques", year, countryCode).SetLaunchYear(1642));
-            items.Add(new Holiday(year, 5, 1, "Fête du Travail", "Labour Day", countryCode));
-            items.Add(this._catholicProvider.AscensionDay("Ascension", year, countryCode));
-            items.Add(new Holiday(year, 5, 8, "Victoire 1945", "Victory in Europe Day", countryCode));
-            items.Add(this._catholicProvider.WhitMonday("Lundi de Pentecôte", year, countryCode));
-            items.Add(new Holiday(year, 7, 14, "Fête nationale", "Bastille Day", countryCode));
-            items.Add(new Holiday(year, 8, 15, "Assomption", "Assumption Day", countryCode));
-            items.Add(new Holiday(year, 11, 1, "Toussaint", "All Saints' Day", countryCode));
-            items.Add(new Holiday(year, 11, 11, "Armistice 1918", "Armistice Day", countryCode));
-            items.Add(new Holiday(year, 12, 25, "Noël", "Christmas Day", countryCode));
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "Jour de l'an",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Fête du Travail",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 8),
+                    EnglishName = "Victory in Europe Day",
+                    LocalName = "Victoire 1945",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 7, 14),
+                    EnglishName = "Bastille Day",
+                    LocalName = "Fête nationale",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 15),
+                    EnglishName = "Assumption Day",
+                    LocalName = "Assomption",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 11, 1),
+                    EnglishName = "All Saints' Day",
+                    LocalName = "Toussaint",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 11, 11),
+                    EnglishName = "Armistice Day",
+                    LocalName = "Armistice 1918",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Noël",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.EasterMonday("Lundi de Pâques", year),
+                this._catholicProvider.AscensionDay("Ascension", year),
+                this._catholicProvider.WhitMonday("Lundi de Pentecôte", year)
+            };
 
-            return items.OrderBy(o => o.Date);
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "Jour de l'an", "New Year's Day", countryCode, 1967));
+            //items.Add(this._catholicProvider.EasterMonday("Lundi de Pâques", year, countryCode).SetLaunchYear(1642));
+            //items.Add(new Holiday(year, 5, 1, "Fête du Travail", "Labour Day", countryCode));
+            //items.Add(this._catholicProvider.AscensionDay("Ascension", year, countryCode));
+            //items.Add(new Holiday(year, 5, 8, "Victoire 1945", "Victory in Europe Day", countryCode));
+            //items.Add(this._catholicProvider.WhitMonday("Lundi de Pentecôte", year, countryCode));
+            //items.Add(new Holiday(year, 7, 14, "Fête nationale", "Bastille Day", countryCode));
+            //items.Add(new Holiday(year, 8, 15, "Assomption", "Assumption Day", countryCode));
+            //items.Add(new Holiday(year, 11, 1, "Toussaint", "All Saints' Day", countryCode));
+            //items.Add(new Holiday(year, 11, 11, "Armistice 1918", "Armistice Day", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Noël", "Christmas Day", countryCode));
+
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
