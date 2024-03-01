2c1f7df2fbbc6b2a4743ee9fbf46d31c0650785f
Tino Hager <tino.hager@nager.at>
2024-03-01 19:36:35 +0100

    migrate Cuba

--- src/Nager.Date/HolidayProviders/CubaHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/CubaHolidayProvider.cs
@@ -51,6 +51,42 @@ namespace Nager.Date.HolidayProviders
                     LocalName = "Día de los trabajadores",
                     HolidayTypes = HolidayTypes.Public
                 },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 7, 25),
+                    EnglishName = "Day before the Commemoration of the Assault of the Moncada garrison",
+                    LocalName = "Conmemoración del asalto a Moncada",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 7, 26),
+                    EnglishName = "Commemoration of the Assault of the Moncada garrison",
+                    LocalName = "Día de la Rebeldía Nacional",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 7, 27),
+                    EnglishName = "Day after the Commemoration of the Assault of the Moncada garrison",
+                    LocalName = "Conmemoración del asalto a Moncada",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 10, 10),
+                    EnglishName = "Day after the Commemoration of the Assault of the Moncada garrison",
+                    LocalName = "Día de la Independencia",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Navidad",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.GoodFriday("Good Friday", year)
             };
 
             var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
@@ -59,15 +95,15 @@ namespace Nager.Date.HolidayProviders
             //var items = new List<Holiday>();
             //items.Add(new Holiday(year, 1, 1, "Triunfo de la Revolución", "Triumph of the Revolution", countryCode));
             //items.Add(new Holiday(year, 1, 2, "Día de Victoria de las Fuerzas Armadas", "Victory of Fidel Castro", countryCode));
-            items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
+            //items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
             //items.Add(new Holiday(year, 5, 1, "Día de los trabajadores", "Labour Day", countryCode));
-            items.Add(new Holiday(year, 7, 25, "Conmemoración del asalto a Moncada", "Day before the Commemoration of the Assault of the Moncada garrison", countryCode));
-            items.Add(new Holiday(year, 7, 26, "Día de la Rebeldía Nacional", "Commemoration of the Assault of the Moncada garrison", countryCode));
-            items.Add(new Holiday(year, 7, 27, "Conmemoración del asalto a Moncada", "Day after the Commemoration of the Assault of the Moncada garrison", countryCode));
-            items.Add(new Holiday(year, 10, 10, "Día de la Independencia", "Independence Day", countryCode));
-            items.Add(new Holiday(year, 12, 25, "Navidad", "Christmas Day", countryCode));       
+            //items.Add(new Holiday(year, 7, 25, "Conmemoración del asalto a Moncada", "Day before the Commemoration of the Assault of the Moncada garrison", countryCode));
+            //items.Add(new Holiday(year, 7, 26, "Día de la Rebeldía Nacional", "Commemoration of the Assault of the Moncada garrison", countryCode));
+            //items.Add(new Holiday(year, 7, 27, "Conmemoración del asalto a Moncada", "Day after the Commemoration of the Assault of the Moncada garrison", countryCode));
+            //items.Add(new Holiday(year, 10, 10, "Día de la Independencia", "Independence Day", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Navidad", "Christmas Day", countryCode));       
 
-            return items.OrderBy(o => o.Date);
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
