3142c52aac6ba0d08979f2368bab4675529ea1fb
Tino Hager <tino.hager@nager.at>
2024-03-01 10:18:33 +0100

    migrate countries

--- src/Nager.Date/Extensions/ListExtension.cs
+++ src/Nager.Date/Extensions/ListExtension.cs
@@ -33,5 +33,34 @@ namespace Nager.Date.Extensions
 
             holidays.AddRange(publicHolidays);
         }
+
+        internal static void AddIfNotNull(
+            this List<HolidaySpecification> holidaySpecificationList,
+            HolidaySpecification holidaySpecification)
+        {
+            if (holidaySpecification == null)
+            {
+                return;
+            }
+
+            holidaySpecificationList.Add(holidaySpecification);
+        }
+
+        internal static void AddRangeIfNotNull(
+            this List<HolidaySpecification> holidaySpecificationList,
+            HolidaySpecification[] holidaySpecifications)
+        {
+            if (holidaySpecifications == null)
+            {
+                return;
+            }
+
+            if (holidaySpecifications.Length == 0)
+            {
+                return;
+            }
+
+            holidaySpecificationList.AddRange(holidaySpecifications);
+        }
     }
 }
--- src/Nager.Date/HolidayProviders/AustriaHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/AustriaHolidayProvider.cs
@@ -1,5 +1,6 @@
 using Nager.Date.Models;
 using Nager.Date.ReligiousProviders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -44,24 +45,98 @@ namespace Nager.Date.HolidayProviders
         {
             var countryCode = CountryCode.AT;
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "Neujahr", "New Year's Day", countryCode, 1967));
-            items.Add(new Holiday(year, 1, 6, "Heilige Drei Könige", "Epiphany", countryCode));
-            //items.Add(new PublicHoliday(year, 3, 19, "St. Josef", "Saint Joseph's Day", countryCode, type: PublicHolidayType.Authorities | PublicHolidayType.School, counties: new string[] { "AT-2", "AT-6", "AT-7", "AT-8" }));
-            items.Add(this._catholicProvider.EasterMonday("Ostermontag", year, countryCode).SetLaunchYear(1642));
-            items.Add(new Holiday(year, 5, 1, "Staatsfeiertag", "National Holiday", countryCode, 1955));
-            //items.Add(new PublicHoliday(year, 5, 1, "St. Florian", "Saint Florian", countryCode, type: PublicHolidayType.School, counties: new string[] { "AT-4" }));
-            items.Add(this._catholicProvider.AscensionDay("Christi Himmelfahrt", year, countryCode));
-            items.Add(this._catholicProvider.WhitMonday("Pfingstmontag", year, countryCode));
-            items.Add(this._catholicProvider.CorpusChristi("Fronleichnam", year, countryCode));
-            items.Add(new Holiday(year, 8, 15, "Maria Himmelfahrt", "Assumption Day", countryCode));
-            items.Add(new Holiday(year, 10, 26, "Nationalfeiertag", "National Holiday", countryCode));
-            items.Add(new Holiday(year, 11, 1, "Allerheiligen", "All Saints' Day", countryCode));
-            items.Add(new Holiday(year, 12, 8, "Mariä Empfängnis", "Immaculate Conception", countryCode));
-            items.Add(new Holiday(year, 12, 25, "Weihnachten", "Christmas Day", countryCode));
-            items.Add(new Holiday(year, 12, 26, "Stefanitag", "St. Stephen's Day", countryCode));
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "Neujahr",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 6),
+                    EnglishName = "Epiphany",
+                    LocalName = "Heilige Drei Könige",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "National Holiday",
+                    LocalName = "Staatsfeiertag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 15),
+                    EnglishName = "Assumption Day",
+                    LocalName = "Maria Himmelfahrt",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 10, 26),
+                    EnglishName = "National Holiday",
+                    LocalName = "Nationalfeiertag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 11, 1),
+                    EnglishName = "All Saints' Day",
+                    LocalName = "Allerheiligen",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 8),
+                    EnglishName = "Immaculate Conception",
+                    LocalName = "Mariä Empfängnis",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Weihnachten",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 26),
+                    EnglishName = "St. Stephen's Day",
+                    LocalName = "Stefanitag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.EasterMonday("Ostermontag", year),
+                this._catholicProvider.AscensionDay("Christi Himmelfahrt", year),
+                this._catholicProvider.WhitMonday("Pfingstmontag", year),
+                this._catholicProvider.CorpusChristi("Fronleichnam", year)
+            };
 
-            return items.OrderBy(o => o.Date);
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "Neujahr", "New Year's Day", countryCode, 1967));
+            //items.Add(new Holiday(year, 1, 6, "Heilige Drei Könige", "Epiphany", countryCode));
+            //items.Add(new PublicHoliday(year, 3, 19, "St. Josef", "Saint Joseph's Day", countryCode, type: PublicHolidayType.Authorities | PublicHolidayType.School, counties: new string[] { "AT-2", "AT-6", "AT-7", "AT-8" }));
+            //items.Add(this._catholicProvider.EasterMonday("Ostermontag", year, countryCode).SetLaunchYear(1642));
+            //items.Add(new Holiday(year, 5, 1, "Staatsfeiertag", "National Holiday", countryCode, 1955));
+            //items.Add(new PublicHoliday(year, 5, 1, "St. Florian", "Saint Florian", countryCode, type: PublicHolidayType.School, counties: new string[] { "AT-4" }));
+            //items.Add(this._catholicProvider.AscensionDay("Christi Himmelfahrt", year, countryCode));
+            //items.Add(this._catholicProvider.WhitMonday("Pfingstmontag", year, countryCode));
+            //items.Add(this._catholicProvider.CorpusChristi("Fronleichnam", year, countryCode));
+            //items.Add(new Holiday(year, 8, 15, "Maria Himmelfahrt", "Assumption Day", countryCode));
+            //items.Add(new Holiday(year, 10, 26, "Nationalfeiertag", "National Holiday", countryCode));
+            //items.Add(new Holiday(year, 11, 1, "Allerheiligen", "All Saints' Day", countryCode));
+            //items.Add(new Holiday(year, 12, 8, "Mariä Empfängnis", "Immaculate Conception", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Weihnachten", "Christmas Day", countryCode));
+            //items.Add(new Holiday(year, 12, 26, "Stefanitag", "St. Stephen's Day", countryCode));
+
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
