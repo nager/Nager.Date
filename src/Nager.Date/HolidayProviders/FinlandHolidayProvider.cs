432c401d3cde36dea3c1a3b5265c624c7b857f24
Tino Hager <tino.hager@nager.at>
2024-03-01 20:54:26 +0100

    migrate Finland

--- src/Nager.Date/HolidayProviders/FinlandHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/FinlandHolidayProvider.cs
@@ -32,24 +32,106 @@ namespace Nager.Date.HolidayProviders
             var midsummerDay = DateSystem.FindDay(year, Month.June, 20, DayOfWeek.Saturday);
             var allSaintsDay = DateSystem.FindDayBetween(year, 10, 31, year, 11, 6, DayOfWeek.Saturday);
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "Uudenvuodenpäivä", "New Year's Day", countryCode));
-            items.Add(new Holiday(year, 1, 6, "Loppiainen", "Epiphany", countryCode));
-            items.Add(this._catholicProvider.GoodFriday("Pitkäperjantai", year, countryCode));
-            items.Add(this._catholicProvider.EasterSunday("Pääsiäispäivä", year, countryCode));
-            items.Add(this._catholicProvider.EasterMonday("Toinen pääsiäispäivä", year, countryCode));
-            items.Add(new Holiday(year, 5, 1, "Vappu", "May Day", countryCode));
-            items.Add(this._catholicProvider.AscensionDay("Helatorstai", year, countryCode));
-            items.Add(this._catholicProvider.Pentecost("Helluntaipäivä", year, countryCode));
-            items.Add(new Holiday(midsummerEve, "Juhannusaatto", "Midsummer Eve", countryCode));
-            items.Add(new Holiday(midsummerDay, "Juhannuspäivä", "Midsummer Day", countryCode));
-            items.Add(new Holiday(allSaintsDay.Value, "Pyhäinpäivä", "All Saints' Day", countryCode));
-            items.Add(new Holiday(year, 12, 6, "Itsenäisyyspäivä", "Independence Day", countryCode));
-            items.Add(new Holiday(year, 12, 24, "Jouluaatto", "Christmas Eve", countryCode));
-            items.Add(new Holiday(year, 12, 25, "Joulupäivä", "Christmas Day", countryCode));
-            items.Add(new Holiday(year, 12, 26, "Tapaninpäivä", "St. Stephen's Day", countryCode));
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "Uudenvuodenpäivä",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 6),
+                    EnglishName = "Epiphany",
+                    LocalName = "Loppiainen",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "May Day",
+                    LocalName = "Vappu",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = midsummerEve,
+                    EnglishName = "Midsummer Eve",
+                    LocalName = "Juhannusaatto",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = midsummerDay,
+                    EnglishName = "Midsummer Day",
+                    LocalName = "Juhannuspäivä",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = allSaintsDay.Value,
+                    EnglishName = "All Saints' Day",
+                    LocalName = "Pyhäinpäivä",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 6),
+                    EnglishName = "Independence Day",
+                    LocalName = "Itsenäisyyspäivä",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 24),
+                    EnglishName = "Christmas Eve",
+                    LocalName = "Jouluaatto",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Joulupäivä",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 26),
+                    EnglishName = "St. Stephen's Day",
+                    LocalName = "Tapaninpäivä",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.GoodFriday("Pitkäperjantai", year),
+                this._catholicProvider.EasterSunday("Pääsiäispäivä", year),
+                this._catholicProvider.EasterMonday("Toinen pääsiäispäivä", year),
+                this._catholicProvider.AscensionDay("Helatorstai", year),
+                this._catholicProvider.Pentecost("Helluntaipäivä", year)
+            };
 
-            return items.OrderBy(o => o.Date);
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "Uudenvuodenpäivä", "New Year's Day", countryCode));
+            //items.Add(new Holiday(year, 1, 6, "Loppiainen", "Epiphany", countryCode));
+            //items.Add(this._catholicProvider.GoodFriday("Pitkäperjantai", year, countryCode));
+            //items.Add(this._catholicProvider.EasterSunday("Pääsiäispäivä", year, countryCode));
+            //items.Add(this._catholicProvider.EasterMonday("Toinen pääsiäispäivä", year, countryCode));
+            //items.Add(new Holiday(year, 5, 1, "Vappu", "May Day", countryCode));
+            //items.Add(this._catholicProvider.AscensionDay("Helatorstai", year, countryCode));
+            //items.Add(this._catholicProvider.Pentecost("Helluntaipäivä", year, countryCode));
+            //items.Add(new Holiday(midsummerEve, "Juhannusaatto", "Midsummer Eve", countryCode));
+            //items.Add(new Holiday(midsummerDay, "Juhannuspäivä", "Midsummer Day", countryCode));
+            //items.Add(new Holiday(allSaintsDay.Value, "Pyhäinpäivä", "All Saints' Day", countryCode));
+            //items.Add(new Holiday(year, 12, 6, "Itsenäisyyspäivä", "Independence Day", countryCode));
+            //items.Add(new Holiday(year, 12, 24, "Jouluaatto", "Christmas Eve", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Joulupäivä", "Christmas Day", countryCode));
+            //items.Add(new Holiday(year, 12, 26, "Tapaninpäivä", "St. Stephen's Day", countryCode));
+
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
