7f513bf7188adf32aef3929026aa69ed487b7803
Tino Hager <tino.hager@nager.at>
2024-03-01 21:48:56 +0100

    migrate Honduras

--- src/Nager.Date/HolidayProviders/HondurasHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/HondurasHolidayProvider.cs
@@ -1,5 +1,6 @@
 using Nager.Date.Models;
 using Nager.Date.ReligiousProviders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -28,20 +29,86 @@ namespace Nager.Date.HolidayProviders
             var countryCode = CountryCode.HN;
             var easterSunday = this._catholicProvider.EasterSunday(year);
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
-            items.Add(new Holiday(year, 4, 14, "America's Day", "America's Day", countryCode));
-            items.Add(this._catholicProvider.MaundyThursday("Holy Thursday", year, countryCode));
-            items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
-            items.Add(new Holiday(easterSunday.AddDays(-1), "Holy Saturday", "Holy Saturday", countryCode));
-            items.Add(new Holiday(year, 5, 1, "Labour Day", "Labour Day", countryCode));
-            items.Add(new Holiday(year, 9, 15, "Independence Day", "Independence Day", countryCode));
-            items.Add(new Holiday(year, 10, 3, "Francisco Morazán's Day/Soldier's Day", "Francisco Morazán's Day/Soldier's Day", countryCode));
-            items.Add(new Holiday(year, 10, 12, "Columbus Day", "Columbus Day", countryCode));
-            items.Add(new Holiday(year, 10, 21, "Army Day", "Army Day", countryCode));
-            items.Add(new Holiday(year, 12, 25, "Navidad", "Christmas Day", countryCode));
-            
-            return items.OrderBy(o => o.Date);
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 4, 14),
+                    EnglishName = "America's Day",
+                    LocalName = "America's Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Labour Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 9, 15),
+                    EnglishName = "Independence Day",
+                    LocalName = "Independence Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 10, 3),
+                    EnglishName = "Francisco Morazán's Day/Soldier's Day",
+                    LocalName = "Francisco Morazán's Day/Soldier's Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 10, 12),
+                    EnglishName = "Columbus Day",
+                    LocalName = "Columbus Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 10, 21),
+                    EnglishName = "Army Day",
+                    LocalName = "Army Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Navidad",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.MaundyThursday("Holy Thursday", year),
+                this._catholicProvider.GoodFriday("Good Friday", year),
+                this._catholicProvider.EasterSaturday("Holy Saturday", year)
+            };
+
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
+            //items.Add(new Holiday(year, 4, 14, "America's Day", "America's Day", countryCode));
+            //items.Add(this._catholicProvider.MaundyThursday("Holy Thursday", year, countryCode));
+            //items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
+            //items.Add(new Holiday(easterSunday.AddDays(-1), "Holy Saturday", "Holy Saturday", countryCode));
+            //items.Add(new Holiday(year, 5, 1, "Labour Day", "Labour Day", countryCode));
+            //items.Add(new Holiday(year, 9, 15, "Independence Day", "Independence Day", countryCode));
+            //items.Add(new Holiday(year, 10, 3, "Francisco Morazán's Day/Soldier's Day", "Francisco Morazán's Day/Soldier's Day", countryCode));
+            //items.Add(new Holiday(year, 10, 12, "Columbus Day", "Columbus Day", countryCode));
+            //items.Add(new Holiday(year, 10, 21, "Army Day", "Army Day", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Navidad", "Christmas Day", countryCode));
+
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
