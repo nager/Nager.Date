2fb8fbfa55309472f4b7a706a7bd962df9e7a36b
Tino Hager <tino.hager@nager.at>
2024-03-02 20:21:28 +0100

    migrate Macedonia

--- src/Nager.Date/HolidayProviders/MacedoniaHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/MacedoniaHolidayProvider.cs
@@ -1,5 +1,6 @@
 using Nager.Date.Models;
 using Nager.Date.ReligiousProviders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -27,22 +28,94 @@ namespace Nager.Date.HolidayProviders
         {
             var countryCode = CountryCode.MK;
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "Нова Година, Nova Godina", "New Year's Day", countryCode));
-            items.Add(new Holiday(year, 1, 7, "Прв ден Божик, Prv den Božik", "Christmas Day", countryCode));
-            items.Add(this._orthodoxProvider.GoodFriday("Велики Петок, Veliki Petok", year, countryCode));
-            items.Add(this._orthodoxProvider.EasterSunday("Прв ден Велигден, Prv den Veligden", year, countryCode));
-            items.Add(this._orthodoxProvider.EasterMonday("Втор ден Велигден, Vtor den Veligden", year, countryCode));
-            items.Add(new Holiday(year, 5, 1, "Ден на трудот, Den na trudot", "Labour Day", countryCode));
-            items.Add(new Holiday(year, 5, 24, "Св. Кирил и Методиј, Ден на сèсловенските просветители", "Saints Cyril and Methodius Day", countryCode));
-            items.Add(new Holiday(year, 8, 2, "Ден на Републиката, Den na Republikata", "Day of the Republic", countryCode));
-            items.Add(new Holiday(year, 9, 8, "Ден на независноста, Den na nezavisnosta", "Independence Day", countryCode));
-            items.Add(new Holiday(year, 10, 11, "Ден на востанието, Den na vostanieto", "Revolution Day", countryCode));
-            items.Add(new Holiday(year, 10, 23, "Ден на македонската револуционерна борба,Den na makedonskata revolucionarna borba", "Day of the Macedonian Revolutionary Struggle", countryCode));
-            items.Add(new Holiday(year, 12, 8, "Св. Климент Охридски, Sv. Kliment Ohridski", "Saint Clement of Ohrid Day", countryCode));
-            //items.Add(new PublicHoliday(year, ??, ??, "Рамазан Бајрам, Ramazan Bajram", "Eid al-Fitr", countryCode)); //Islamic
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "Нова Година, Nova Godina",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 7),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Прв ден Божик, Prv den Božik",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Ден на трудот, Den na trudot",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 24),
+                    EnglishName = "Saints Cyril and Methodius Day",
+                    LocalName = "Св. Кирил и Методиј, Ден на сèсловенските просветители",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 2),
+                    EnglishName = "Day of the Republic",
+                    LocalName = "Ден на Републиката, Den na Republikata",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 9, 8),
+                    EnglishName = "Independence Day",
+                    LocalName = "Ден на независноста, Den na nezavisnosta",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 10, 11),
+                    EnglishName = "Revolution Day",
+                    LocalName = "Ден на востанието, Den na vostanieto",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 10, 23),
+                    EnglishName = "Day of the Macedonian Revolutionary Struggle",
+                    LocalName = "Ден на македонската револуционерна борба,Den na makedonskata revolucionarna borba",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 8),
+                    EnglishName = "Saint Clement of Ohrid Day",
+                    LocalName = "Св. Климент Охридски, Sv. Kliment Ohridski",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._orthodoxProvider.GoodFriday("Велики Петок, Veliki Petok", year),
+                this._orthodoxProvider.EasterSunday("Прв ден Велигден, Prv den Veligden", year),
+                this._orthodoxProvider.EasterMonday("Втор ден Велигден, Vtor den Veligden", year)
+            };
 
-            return items.OrderBy(o => o.Date);
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "Нова Година, Nova Godina", "New Year's Day", countryCode));
+            //items.Add(new Holiday(year, 1, 7, "Прв ден Божик, Prv den Božik", "Christmas Day", countryCode));
+            //items.Add(this._orthodoxProvider.GoodFriday("Велики Петок, Veliki Petok", year, countryCode));
+            //items.Add(this._orthodoxProvider.EasterSunday("Прв ден Велигден, Prv den Veligden", year, countryCode));
+            //items.Add(this._orthodoxProvider.EasterMonday("Втор ден Велигден, Vtor den Veligden", year, countryCode));
+            //items.Add(new Holiday(year, 5, 1, "Ден на трудот, Den na trudot", "Labour Day", countryCode));
+            //items.Add(new Holiday(year, 5, 24, "Св. Кирил и Методиј, Ден на сèсловенските просветители", "Saints Cyril and Methodius Day", countryCode));
+            //items.Add(new Holiday(year, 8, 2, "Ден на Републиката, Den na Republikata", "Day of the Republic", countryCode));
+            //items.Add(new Holiday(year, 9, 8, "Ден на независноста, Den na nezavisnosta", "Independence Day", countryCode));
+            //items.Add(new Holiday(year, 10, 11, "Ден на востанието, Den na vostanieto", "Revolution Day", countryCode));
+            //items.Add(new Holiday(year, 10, 23, "Ден на македонската револуционерна борба,Den na makedonskata revolucionarna borba", "Day of the Macedonian Revolutionary Struggle", countryCode));
+            //items.Add(new Holiday(year, 12, 8, "Св. Климент Охридски, Sv. Kliment Ohridski", "Saint Clement of Ohrid Day", countryCode));
+            //items.Add(new PublicHoliday(year, ??, ??, "Рамазан Бајрам, Ramazan Bajram", "Eid al-Fitr", countryCode)); //Islamic
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
