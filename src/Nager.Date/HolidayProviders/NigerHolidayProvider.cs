bea8394967a092dabd308c76cc48cc8d56be10a8
Tino Hager <tino.hager@nager.at>
2024-03-02 21:01:20 +0100

    migrate Niger

--- src/Nager.Date/HolidayProviders/NigerHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/NigerHolidayProvider.cs
@@ -1,5 +1,6 @@
 using Nager.Date.Models;
 using Nager.Date.ReligiousProviders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -26,19 +27,68 @@ namespace Nager.Date.HolidayProviders
         public IEnumerable<Holiday> GetHolidays(int year)
         {
             var countryCode = CountryCode.NE;
- 
+
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 4, 24),
+                    EnglishName = "Concord Day",
+                    LocalName = "Concord Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Labour Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 3),
+                    EnglishName = "Nigerien Independence Day",
+                    LocalName = "Nigerien Independence Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 18),
+                    EnglishName = "Nigerien Republic Day",
+                    LocalName = "Nigerien Republic Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.EasterMonday("Easter Monday", year)
+            };
+
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
             //TODO: Add islamic public holidays
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
-            items.Add(new Holiday(year, 4, 24, "Concord Day", "Concord Day", countryCode));
-            items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
-            items.Add(new Holiday(year, 5, 1, "Labour Day", "Labour Day", countryCode));
-            items.Add(new Holiday(year, 8, 3, "Nigerien Independence Day", "Nigerien Independence Day", countryCode));
-            items.Add(new Holiday(year, 12, 18, "Nigerien Republic Day", "Nigerien Republic Day", countryCode));
-            items.Add(new Holiday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
-
-            return items.OrderBy(o => o.Date);
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
+            //items.Add(new Holiday(year, 4, 24, "Concord Day", "Concord Day", countryCode));
+            //items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
+            //items.Add(new Holiday(year, 5, 1, "Labour Day", "Labour Day", countryCode));
+            //items.Add(new Holiday(year, 8, 3, "Nigerien Independence Day", "Nigerien Independence Day", countryCode));
+            //items.Add(new Holiday(year, 12, 18, "Nigerien Republic Day", "Nigerien Republic Day", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
