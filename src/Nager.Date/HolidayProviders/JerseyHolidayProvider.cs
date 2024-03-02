e72c1e7ae26ad31b0da85a2a2847715fa197bb13
Tino Hager <tino.hager@nager.at>
2024-03-02 19:39:44 +0100

    migrate Jersey

--- src/Nager.Date/HolidayProviders/JerseyHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/JerseyHolidayProvider.cs
@@ -32,18 +32,76 @@ namespace Nager.Date.HolidayProviders
             var lastMondayInMay = DateSystem.FindLastDay(year, Month.May, DayOfWeek.Monday);
             var lastMondayInAugust = DateSystem.FindLastDay(year, Month.August, DayOfWeek.Monday);
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
-            items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
-            items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
-            items.Add(new Holiday(firstMondayInMay, "Early May Bank Holiday", "Early May Bank Holiday", countryCode));
-            items.Add(new Holiday(year, 5, 9, "Liberation Day", "Liberation Day", countryCode));
-            items.Add(new Holiday(lastMondayInMay, "Spring Bank Holiday", "Spring Bank Holiday", countryCode));
-            items.Add(new Holiday(lastMondayInAugust, "Summer Bank Holiday", "Summer Bank Holiday", countryCode));
-            items.Add(new Holiday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
-            items.Add(new Holiday(year, 12, 26, "Boxing Day", "St. Stephen's Day", countryCode));
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = firstMondayInMay,
+                    EnglishName = "Early May Bank Holiday",
+                    LocalName = "Early May Bank Holiday",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 9),
+                    EnglishName = "Liberation Day",
+                    LocalName = "Liberation Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = lastMondayInMay,
+                    EnglishName = "Spring Bank Holiday",
+                    LocalName = "Spring Bank Holiday",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = lastMondayInAugust,
+                    EnglishName = "Summer Bank Holiday",
+                    LocalName = "Summer Bank Holiday",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 26),
+                    EnglishName = "Boxing Day",
+                    LocalName = "St. Stephen's Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.GoodFriday("Good Friday", year),
+                this._catholicProvider.EasterMonday("Easter Monday", year)
+            };
 
-            return items.OrderBy(o => o.Date);
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
+            //items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
+            //items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
+            //items.Add(new Holiday(firstMondayInMay, "Early May Bank Holiday", "Early May Bank Holiday", countryCode));
+            //items.Add(new Holiday(year, 5, 9, "Liberation Day", "Liberation Day", countryCode));
+            //items.Add(new Holiday(lastMondayInMay, "Spring Bank Holiday", "Spring Bank Holiday", countryCode));
+            //items.Add(new Holiday(lastMondayInAugust, "Summer Bank Holiday", "Summer Bank Holiday", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
+            //items.Add(new Holiday(year, 12, 26, "Boxing Day", "St. Stephen's Day", countryCode));
+
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
