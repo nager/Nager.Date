4e85c79ed64007f8ff0912913e67b60cadf54070
Tino Hager <tino.hager@nager.at>
2024-03-02 19:20:32 +0100

    migrate Jamaica

--- src/Nager.Date/HolidayProviders/JamaicaHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/JamaicaHolidayProvider.cs
@@ -30,26 +30,95 @@ namespace Nager.Date.HolidayProviders
             var countryCode = CountryCode.JM;
             var easterSunday = this._catholicProvider.EasterSunday(year);
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
-            items.Add(new Holiday(easterSunday.AddDays(-46), "Ash Wednesday", "Ash Wednesday", countryCode));
-            items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
-            items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
-            items.Add(new Holiday(year, 5, 23, "Labour Day", "Labour Day", countryCode));
-            items.Add(new Holiday(year, 8, 1, "Emancipation Day", "Emancipation Day", countryCode));
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 23),
+                    EnglishName = "Labour Day",
+                    LocalName = "Labour Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 1),
+                    EnglishName = "Emancipation Day",
+                    LocalName = "Emancipation Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 6),
+                    EnglishName = "Independence Day",
+                    LocalName = "Independence Day",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = new ObservedRuleSet
+                    {
+                        Sunday = date => date.AddDays(1)
+                    }
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 10, 16),
+                    EnglishName = "National Heroes Day",
+                    LocalName = "National Heroes Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 26),
+                    EnglishName = "St. Stephen's Day",
+                    LocalName = "Boxing Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = easterSunday.AddDays(-46),
+                    EnglishName = "Ash Wednesday",
+                    LocalName = "Ash Wednesday",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.GoodFriday("Good Friday", year),
+                this._catholicProvider.EasterMonday("Easter Monday", year)
+            };
 
-            #region Independence Day
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
 
-            var independenceDay = new DateTime(year, 8, 6).Shift(saturday => saturday, sunday => sunday.AddDays(1));
-            items.Add(new Holiday(independenceDay, "Independence Day", "Independence Day", countryCode));
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
+            //items.Add(new Holiday(easterSunday.AddDays(-46), "Ash Wednesday", "Ash Wednesday", countryCode));
+            //items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
+            //items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
+            //items.Add(new Holiday(year, 5, 23, "Labour Day", "Labour Day", countryCode));
+            //items.Add(new Holiday(year, 8, 1, "Emancipation Day", "Emancipation Day", countryCode));
 
-            #endregion
+            //#region Independence Day
 
-            items.Add(new Holiday(year, 10, 16, "National Heroes Day", "National Heroes Day", countryCode));
-            items.Add(new Holiday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
-            items.Add(new Holiday(year, 12, 26, "Boxing Day", "St. Stephen's Day", countryCode));            
+            //var independenceDay = new DateTime(year, 8, 6).Shift(saturday => saturday, sunday => sunday.AddDays(1));
+            //items.Add(new Holiday(independenceDay, "Independence Day", "Independence Day", countryCode));
 
-            return items.OrderBy(o => o.Date);
+            //#endregion
+
+            //items.Add(new Holiday(year, 10, 16, "National Heroes Day", "National Heroes Day", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
+            //items.Add(new Holiday(year, 12, 26, "Boxing Day", "St. Stephen's Day", countryCode));            
+
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
