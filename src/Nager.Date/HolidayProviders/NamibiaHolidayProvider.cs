33bb44a4a8fcb89090a54ef9677cbb7d64c84add
Tino Hager <tino.hager@nager.at>
2024-03-02 21:06:47 +0100

    migrate Namibia

--- src/Nager.Date/HolidayProviders/NamibiaHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/NamibiaHolidayProvider.cs
@@ -1,5 +1,6 @@
 using Nager.Date.Models;
 using Nager.Date.ReligiousProviders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -26,21 +27,93 @@ namespace Nager.Date.HolidayProviders
         {
             var countryCode = CountryCode.NA;
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
-            items.Add(new Holiday(year, 3, 21, "Independence Day", "Independence Day", countryCode));
-            items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
-            items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
-            items.Add(new Holiday(year, 5, 1, "Workers' Day", "Workers' Day", countryCode));
-            items.Add(new Holiday(year, 5, 4, "Cassinga Day", "Cassinga Day", countryCode));
-            items.Add(this._catholicProvider.AscensionDay("Ascension Day", year, countryCode));
-            items.Add(new Holiday(year, 5, 25, "Africa Day", "Africa Day", countryCode));
-            items.Add(new Holiday(year, 8, 26, "Heroes' Day", "Heroes' Day", countryCode));
-            items.Add(new Holiday(year, 12, 10, "Human Rights Day", "Human Rights Day", countryCode));
-            items.Add(new Holiday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
-            items.Add(new Holiday(year, 12, 26, "Day of Goodwill", "St. Stephen's Day", countryCode));
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 3, 21),
+                    EnglishName = "Independence Day",
+                    LocalName = "Independence Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Workers' Day",
+                    LocalName = "Workers' Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 4),
+                    EnglishName = "Cassinga Day",
+                    LocalName = "Cassinga Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 25),
+                    EnglishName = "Africa Day",
+                    LocalName = "Africa Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 26),
+                    EnglishName = "Heroes' Day",
+                    LocalName = "Heroes' Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 10),
+                    EnglishName = "Human Rights Day",
+                    LocalName = "Human Rights Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 26),
+                    EnglishName = "Day of Goodwill",
+                    LocalName = "St. Stephen's Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.GoodFriday("Good Friday", year),
+                this._catholicProvider.EasterMonday("Easter Monday", year),
+                this._catholicProvider.AscensionDay("Ascension Day", year)
+            };
 
-            return items.OrderBy(o => o.Date);
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "New Year's Day", "New Year's Day", countryCode));
+            //items.Add(new Holiday(year, 3, 21, "Independence Day", "Independence Day", countryCode));
+            //items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
+            //items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
+            //items.Add(new Holiday(year, 5, 1, "Workers' Day", "Workers' Day", countryCode));
+            //items.Add(new Holiday(year, 5, 4, "Cassinga Day", "Cassinga Day", countryCode));
+            //items.Add(this._catholicProvider.AscensionDay("Ascension Day", year, countryCode));
+            //items.Add(new Holiday(year, 5, 25, "Africa Day", "Africa Day", countryCode));
+            //items.Add(new Holiday(year, 8, 26, "Heroes' Day", "Heroes' Day", countryCode));
+            //items.Add(new Holiday(year, 12, 10, "Human Rights Day", "Human Rights Day", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Christmas Day", "Christmas Day", countryCode));
+            //items.Add(new Holiday(year, 12, 26, "Day of Goodwill", "St. Stephen's Day", countryCode));
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
