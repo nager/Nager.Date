733563304d8e955ec340bc147d13b4b4e6fa99e5
Tino Hager <tino.hager@nager.at>
2024-03-02 20:17:40 +0100

    migrate Luxembourg

--- src/Nager.Date/HolidayProviders/LuxembourgHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/LuxembourgHolidayProvider.cs
@@ -1,5 +1,6 @@
 using Nager.Date.Models;
 using Nager.Date.ReligiousProviders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -27,21 +28,87 @@ namespace Nager.Date.HolidayProviders
         {
             var countryCode = CountryCode.LU;
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "Neijoerschdag", "New Year's Day", countryCode));
-            items.Add(this._catholicProvider.EasterMonday("Ouschterméindeg", year, countryCode));
-            items.Add(new Holiday(year, 5, 1, "Dag vun der Aarbecht", "Labour Day", countryCode));
-            items.Add(new Holiday(year, 5, 9, "Europadag", "Europe Day", countryCode, 2019));
-            items.Add(this._catholicProvider.GoodFriday("Karfreideg", year, countryCode).SetType(HolidayTypes.Bank));
-            items.Add(this._catholicProvider.AscensionDay("Christi Himmelfaart", year, countryCode));
-            items.Add(this._catholicProvider.WhitMonday("Péngschtméindeg", year, countryCode));
-            items.Add(new Holiday(year, 6, 23, "Groussherzogsgebuertsdag", "Sovereign's birthday", countryCode));
-            items.Add(new Holiday(year, 8, 15, "Léiffrawëschdag", "Assumption Day", countryCode));
-            items.Add(new Holiday(year, 11, 1, "Allerhellgen", "All Saints' Day", countryCode));
-            items.Add(new Holiday(year, 12, 25, "Chrëschtdag", "Christmas Day", countryCode));
-            items.Add(new Holiday(year, 12, 26, "Stiefesdag", "St. Stephen's Day", countryCode));
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "Neijoerschdag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Dag vun der Aarbecht",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 9),
+                    EnglishName = "Europe Day",
+                    LocalName = "Europadag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 6, 23),
+                    EnglishName = "Sovereign's birthday",
+                    LocalName = "Groussherzogsgebuertsdag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 15),
+                    EnglishName = "Assumption Day",
+                    LocalName = "Léiffrawëschdag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 11, 1),
+                    EnglishName = "All Saints' Day",
+                    LocalName = "Allerhellgen",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Chrëschtdag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 26),
+                    EnglishName = "St. Stephen's Day",
+                    LocalName = "Stiefesdag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.EasterMonday("Ouschterméindeg", year),
+                this._catholicProvider.GoodFriday("Karfreideg", year).SetHolidayType(HolidayTypes.Bank),
+                this._catholicProvider.AscensionDay("Christi Himmelfaart", year),
+                this._catholicProvider.WhitMonday("Péngschtméindeg", year)
+            };
 
-            return items.OrderBy(o => o.Date);
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "Neijoerschdag", "New Year's Day", countryCode));
+            //items.Add(this._catholicProvider.EasterMonday("Ouschterméindeg", year, countryCode));
+            //items.Add(new Holiday(year, 5, 1, "Dag vun der Aarbecht", "Labour Day", countryCode));
+            //items.Add(new Holiday(year, 5, 9, "Europadag", "Europe Day", countryCode, 2019));
+            //items.Add(this._catholicProvider.GoodFriday("Karfreideg", year, countryCode).SetType(HolidayTypes.Bank));
+            //items.Add(this._catholicProvider.AscensionDay("Christi Himmelfaart", year, countryCode));
+            //items.Add(this._catholicProvider.WhitMonday("Péngschtméindeg", year, countryCode));
+            //items.Add(new Holiday(year, 6, 23, "Groussherzogsgebuertsdag", "Sovereign's birthday", countryCode));
+            //items.Add(new Holiday(year, 8, 15, "Léiffrawëschdag", "Assumption Day", countryCode));
+            //items.Add(new Holiday(year, 11, 1, "Allerhellgen", "All Saints' Day", countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Chrëschtdag", "Christmas Day", countryCode));
+            //items.Add(new Holiday(year, 12, 26, "Stiefesdag", "St. Stephen's Day", countryCode));
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
