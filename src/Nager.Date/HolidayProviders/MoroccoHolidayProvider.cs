28ee7a2075eec9e9a7133240534852bc9f006ca0
Tino Hager <tino.hager@nager.at>
2024-03-02 21:11:09 +0100

    migrate Morocco

--- src/Nager.Date/HolidayProviders/MoroccoHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/MoroccoHolidayProvider.cs
@@ -1,4 +1,5 @@
 using Nager.Date.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -27,18 +28,87 @@ namespace Nager.Date.HolidayProviders
             //Eid ul-Fitr (Eid Sghir)
             //Eid ul-Adha (Eid Kbir)
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "Ras l' âm", "New Year's Day", countryCode));
-            items.Add(new Holiday(year, 1, 11, "Takdim watikat al-istiqlal", "Proclamation of Independence", countryCode, 1956));
-            items.Add(new Holiday(year, 5, 1, "Eid Ash-Shughl", "Labour Day", countryCode));
-            items.Add(new Holiday(year, 7, 30, "Eid Al-Ârch", "Enthronement", countryCode));
-            items.Add(new Holiday(year, 8, 14, "Oued Ed-Dahab Day", "Zikra Oued Ed-Dahab", countryCode));
-            items.Add(new Holiday(year, 8, 20, "Thawrat al malik wa shâab", "Revolution of the King and the People", countryCode));
-            items.Add(new Holiday(year, 8, 21, "Eid Al Chabab", "Youth Day", countryCode));
-            items.Add(new Holiday(year, 11, 6, "Eid Al Massira Al Khadra", "Green March", countryCode));
-            items.Add(new Holiday(year, 11, 18, "Eid Al Istiqulal", "Independence Day", countryCode));
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "Ras l' âm",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 11),
+                    EnglishName = "Proclamation of Independence",
+                    LocalName = "Takdim watikat al-istiqlal",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Eid Ash-Shughl",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 7, 30),
+                    EnglishName = "Enthronement",
+                    LocalName = "Eid Al-Ârch",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 14),
+                    EnglishName = "Zikra Oued Ed-Dahab",
+                    LocalName = "Oued Ed-Dahab Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 20),
+                    EnglishName = "Revolution of the King and the People",
+                    LocalName = "Thawrat al malik wa shâab",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 21),
+                    EnglishName = "Youth Day",
+                    LocalName = "Eid Al Chabab",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 11, 6),
+                    EnglishName = "Green March",
+                    LocalName = "Eid Al Massira Al Khadra",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 11, 18),
+                    EnglishName = "Independence Day",
+                    LocalName = "Eid Al Istiqulal",
+                    HolidayTypes = HolidayTypes.Public
+                }
+            };
 
-            return items.OrderBy(o => o.Date);
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "Ras l' âm", "New Year's Day", countryCode));
+            //items.Add(new Holiday(year, 1, 11, "Takdim watikat al-istiqlal", "Proclamation of Independence", countryCode, 1956));
+            //items.Add(new Holiday(year, 5, 1, "Eid Ash-Shughl", "Labour Day", countryCode));
+            //items.Add(new Holiday(year, 7, 30, "Eid Al-Ârch", "Enthronement", countryCode));
+            //items.Add(new Holiday(year, 8, 14, "Oued Ed-Dahab Day", "Zikra Oued Ed-Dahab", countryCode));
+            //items.Add(new Holiday(year, 8, 20, "Thawrat al malik wa shâab", "Revolution of the King and the People", countryCode));
+            //items.Add(new Holiday(year, 8, 21, "Eid Al Chabab", "Youth Day", countryCode));
+            //items.Add(new Holiday(year, 11, 6, "Eid Al Massira Al Khadra", "Green March", countryCode));
+            //items.Add(new Holiday(year, 11, 18, "Eid Al Istiqulal", "Independence Day", countryCode));
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
