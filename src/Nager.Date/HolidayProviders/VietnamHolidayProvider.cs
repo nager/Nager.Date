f0874991a12f86bf56d8856605e45ae7bf7ef3e6
Tino Hager <tino.hager@nager.at>
2024-03-03 20:41:52 +0100

    migrate Vietnam

--- src/Nager.Date/HolidayProviders/VietnamHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/VietnamHolidayProvider.cs
@@ -1,4 +1,5 @@
 using Nager.Date.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -25,13 +26,47 @@ namespace Nager.Date.HolidayProviders
 
             var countryCode = CountryCode.VN;
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "Tết dương lịch", "New Year's Day", countryCode));
-            items.Add(new Holiday(year, 4, 30, "Ngày Giải phóng miền Nam, thống nhất đất nước", "Reunification Day", countryCode));
-            items.Add(new Holiday(year, 5, 1, "Ngày Quốc tế lao động", "Labour Day", countryCode));
-            items.Add(new Holiday(year, 9, 2, "Quốc khánh", "National Day", countryCode));
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "Tết dương lịch",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 4, 30),
+                    EnglishName = "Reunification Day",
+                    LocalName = "Ngày Giải phóng miền Nam, thống nhất đất nước",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Ngày Quốc tế lao động",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 9, 2),
+                    EnglishName = "National Day",
+                    LocalName = "Quốc khánh",
+                    HolidayTypes = HolidayTypes.Public
+                }
+            };
 
-            return items.OrderBy(o => o.Date);
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "Tết dương lịch", "New Year's Day", countryCode));
+            //items.Add(new Holiday(year, 4, 30, "Ngày Giải phóng miền Nam, thống nhất đất nước", "Reunification Day", countryCode));
+            //items.Add(new Holiday(year, 5, 1, "Ngày Quốc tế lao động", "Labour Day", countryCode));
+            //items.Add(new Holiday(year, 9, 2, "Quốc khánh", "National Day", countryCode));
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
