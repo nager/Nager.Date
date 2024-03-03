0c0066e2ea591eccb46a26a41f99ce358b9234a8
Tino Hager <tino.hager@nager.at>
2024-03-03 21:05:38 +0100

    migrate Netherlands

--- src/Nager.Date/HolidayProviders/NetherlandsHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/NetherlandsHolidayProvider.cs
@@ -1,3 +1,4 @@
+using Nager.Date.Extensions;
 using Nager.Date.Models;
 using Nager.Date.ReligiousProviders;
 using System;
@@ -28,49 +29,112 @@ namespace Nager.Date.HolidayProviders
         {
             var countryCode = CountryCode.NL;
 
-            #region King's Day is Sunday fallback
-
-            var kingsDay = 27;
-            var kingsDate = new DateTime(year, 4, kingsDay);
-            if (kingsDate.DayOfWeek == DayOfWeek.Sunday)
+            var observedRuleSet = new ObservedRuleSet
             {
-                kingsDay = 26;
-            }
+                Sunday = date => date.AddDays(-1)
+            };
 
-            #endregion
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "Nieuwjaarsdag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 4, 27),
+                    EnglishName = "King's Day",
+                    LocalName = "Koningsdag",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = observedRuleSet
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Eerste Kerstdag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 26),
+                    EnglishName = "St. Stephen's Day",
+                    LocalName = "Tweede Kerstdag",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                this._catholicProvider.GoodFriday("Goede Vrijdag", year),
+                this._catholicProvider.EasterSunday("Eerste Paasdag", year),
+                this._catholicProvider.EasterMonday("Tweede Paasdag", year),
+                this._catholicProvider.AscensionDay("Hemelvaartsdag", year),
+                this._catholicProvider.Pentecost("Eerste Pinksterdag", year),
+                this._catholicProvider.WhitMonday("Tweede Pinksterdag", year)
+            };
 
-            var items = new List<Holiday>();
-            items.Add(new Holiday(year, 1, 1, "Nieuwjaarsdag", "New Year's Day", countryCode, 1967));
-            items.Add(this._catholicProvider.GoodFriday("Goede Vrijdag", year, countryCode));
-            items.Add(this._catholicProvider.EasterSunday("Eerste Paasdag", year, countryCode));
-            items.Add(this._catholicProvider.EasterMonday("Tweede Paasdag", year, countryCode).SetLaunchYear(1642));
-            items.Add(new Holiday(year, 4, kingsDay, "Koningsdag", "King's Day", countryCode));
-            items.Add(this._catholicProvider.AscensionDay("Hemelvaartsdag", year, countryCode));
-            items.Add(this._catholicProvider.Pentecost("Eerste Pinksterdag", year, countryCode));
-            items.Add(this._catholicProvider.WhitMonday("Tweede Pinksterdag", year, countryCode));
-            items.Add(new Holiday(year, 12, 25, "Eerste Kerstdag", "Christmas Day", countryCode));
-            items.Add(new Holiday(year, 12, 26, "Tweede Kerstdag", "St. Stephen's Day", countryCode));
+            holidaySpecifications.AddIfNotNull(this.LiberationDay(year));
 
-            #region Liberation Day
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
 
-            var liberationDay = new Holiday(year, 5, 5, "Bevrijdingsdag", "Liberation Day", countryCode, 1945);
+
+
+
+            //#region King's Day is Sunday fallback
+
+            //var kingsDay = 27;
+            //var kingsDate = new DateTime(year, 4, kingsDay);
+            //if (kingsDate.DayOfWeek == DayOfWeek.Sunday)
+            //{
+            //    kingsDay = 26;
+            //}
+
+            //#endregion
+
+            //var items = new List<Holiday>();
+            //items.Add(new Holiday(year, 1, 1, "Nieuwjaarsdag", "New Year's Day", countryCode, 1967));
+            //items.Add(this._catholicProvider.GoodFriday("Goede Vrijdag", year, countryCode));
+            //items.Add(this._catholicProvider.EasterSunday("Eerste Paasdag", year, countryCode));
+            //items.Add(this._catholicProvider.EasterMonday("Tweede Paasdag", year, countryCode).SetLaunchYear(1642));
+            //items.Add(new Holiday(year, 4, kingsDay, "Koningsdag", "King's Day", countryCode));
+            //items.Add(this._catholicProvider.AscensionDay("Hemelvaartsdag", year, countryCode));
+            //items.Add(this._catholicProvider.Pentecost("Eerste Pinksterdag", year, countryCode));
+            //items.Add(this._catholicProvider.WhitMonday("Tweede Pinksterdag", year, countryCode));
+            //items.Add(new Holiday(year, 12, 25, "Eerste Kerstdag", "Christmas Day", countryCode));
+            //items.Add(new Holiday(year, 12, 26, "Tweede Kerstdag", "St. Stephen's Day", countryCode));
+            //return items.OrderBy(o => o.Date);
+        }
+
+        private HolidaySpecification LiberationDay(int year)
+        {
+            var liberationDay = new HolidaySpecification
+            {
+                Date = new DateTime(year, 5, 5),
+                EnglishName = "Liberation Day",
+                LocalName = "Bevrijdingsdag",
+                HolidayTypes = HolidayTypes.Public
+            };
+
+            //var liberationDay = new Holiday(year, 5, 5, "Bevrijdingsdag", "Liberation Day", countryCode, 1945);
 
             if (year >= 1990)
             {
                 //in 1990, the day was declared to be a national holiday
-                items.Add(liberationDay.SetType(HolidayTypes.Authorities | HolidayTypes.School));
+                return liberationDay.SetHolidayType(HolidayTypes.Authorities | HolidayTypes.School)
+
+                //items.Add(liberationDay.SetType(HolidayTypes.Authorities | HolidayTypes.School));
             }
             else if (year >= 1945)
             {
                 if (year % 5 == 0)
                 {
-                    items.Add(liberationDay);
+                    return liberationDay;
+                    //items.Add(liberationDay);
                 }
             }
 
-            #endregion
-
-            return items.OrderBy(o => o.Date);
+            return null;
         }
 
         /// <inheritdoc/>
