936e49bac686ee3f0e2f9c696f9769be6b0a9909
Tino Hager <tino.hager@nager.at>
2024-03-04 13:59:25 +0100

    migrate Isle of Man

--- src/Nager.Date/HolidayProviders/IsleOfManHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/IsleOfManHolidayProvider.cs
@@ -15,7 +15,7 @@ namespace Nager.Date.HolidayProviders
         private readonly ICatholicProvider _catholicProvider;
 
         /// <summary>
-        /// IsleOfMan HolidayProvider
+        /// Isle of Man HolidayProvider
         /// </summary>
         /// <param name="catholicProvider"></param>
         public IsleOfManHolidayProvider(
@@ -29,54 +29,127 @@ namespace Nager.Date.HolidayProviders
         {
             var countryCode = CountryCode.IM;
 
+            var observedRuleSet1 = new ObservedRuleSet
+            {
+                Saturday = date => date.AddDays(2),
+                Sunday = date => date.AddDays(1),
+            };
+
+            var observedRuleSet2 = new ObservedRuleSet
+            {
+                Saturday = date => date.AddDays(2),
+                Sunday = date => date.AddDays(2),
+            };
+
             var lastMondayInAugust = DateSystem.FindLastDay(year, Month.August, DayOfWeek.Monday);
-            var tynwaldDay = new DateTime(year, 7, 5).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(1));
-            var christmasDay = new DateTime(year, 12, 25).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(2));
-            var stStephensDay = new DateTime(year, 12, 26).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(2));
+            //var tynwaldDay = new DateTime(year, 7, 5).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(1));
+            //var christmasDay = new DateTime(year, 12, 25).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(2));
+            //var stStephensDay = new DateTime(year, 12, 26).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(2));
+            //var newYearDay = new DateTime(year, 1, 1).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(1));
 
-            var items = new List<Holiday>();
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = observedRuleSet1
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 7, 5),
+                    EnglishName = "Tynwald Day",
+                    LocalName = "Tynwald Day",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = observedRuleSet1
+                },
+                new HolidaySpecification
+                {
+                    Date = lastMondayInAugust,
+                    EnglishName = "Late Summer Bank Holiday",
+                    LocalName = "Late Summer Bank Holiday",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = observedRuleSet2
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 26),
+                    EnglishName = "Boxing Day",
+                    LocalName = "St. Stephen's Day",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = observedRuleSet2
+                },
+                this._catholicProvider.GoodFriday("Good Friday", year),
+                this._catholicProvider.EasterMonday("Easter Monday", year)
+            };
 
-            #region New Year's Day with fallback
+            holidaySpecifications.AddIfNotNull(this.EarlyMayBankHoliday(year));
+            holidaySpecifications.AddIfNotNull(this.SpringBankHoliday(year));
+            holidaySpecifications.AddIfNotNull(this.QueensPlatinumJubilee(year));
+            holidaySpecifications.AddIfNotNull(this.QueensStateFuneral(year));
+            holidaySpecifications.AddIfNotNull(this.CoronationBankHoliday(year));
+            holidaySpecifications.AddIfNotNull(this.SeniorRaceDay(year));
 
-            var newYearDay = new DateTime(year, 1, 1).Shift(saturday => saturday.AddDays(2), sunday => sunday.AddDays(1));
-            items.Add(new Holiday(newYearDay, "New Year's Day", "New Year's Day", countryCode));
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
 
-            #endregion
-
-            items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
-            items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
-
-            items.Add(new Holiday(tynwaldDay, "Tynwald Day", "Tynwald Day", countryCode));
-            items.Add(new Holiday(lastMondayInAugust, "Late Summer Bank Holiday", "Late Summer Bank Holiday", countryCode));
-            items.Add(new Holiday(christmasDay, "Christmas Day", "Christmas Day", countryCode));
-            items.Add(new Holiday(stStephensDay, "Boxing Day", "St. Stephen's Day", countryCode));
-
-            items.AddIfNotNull(this.EarlyMayBankHoliday(year, countryCode));
-            items.AddIfNotNull(this.SpringBankHoliday(year, countryCode));
-            items.AddIfNotNull(this.QueensPlatinumJubilee(year, countryCode));
-            items.AddIfNotNull(this.QueensStateFuneral(year, countryCode));
-            items.AddIfNotNull(this.CoronationBankHoliday(year, countryCode));
-            items.AddIfNotNull(this.SeniorRaceDay(year, countryCode));
-
-            return items.OrderBy(o => o.Date);
+//            //var items = new List<Holiday>();
+            //items.Add(new Holiday(newYearDay, "New Year's Day", "New Year's Day", countryCode));
+            //items.Add(new Holiday(tynwaldDay, "Tynwald Day", "Tynwald Day", countryCode));
+            //items.Add(new Holiday(lastMondayInAugust, "Late Summer Bank Holiday", "Late Summer Bank Holiday", countryCode));
+            //items.Add(new Holiday(christmasDay, "Christmas Day", "Christmas Day", countryCode));
+            //items.Add(new Holiday(stStephensDay, "Boxing Day", "St. Stephen's Day", countryCode));
+            //items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode));
+            //items.Add(this._catholicProvider.EasterMonday("Easter Monday", year, countryCode));
+            //items.AddIfNotNull(this.EarlyMayBankHoliday(year, countryCode));
+            //items.AddIfNotNull(this.SpringBankHoliday(year, countryCode));
+            //items.AddIfNotNull(this.QueensPlatinumJubilee(year, countryCode));
+            //items.AddIfNotNull(this.QueensStateFuneral(year, countryCode));
+            //items.AddIfNotNull(this.CoronationBankHoliday(year, countryCode));
+            //items.AddIfNotNull(this.SeniorRaceDay(year, countryCode));
+            //return items.OrderBy(o => o.Date);
         }
 
-        private Holiday SeniorRaceDay(int year, CountryCode countryCode)
+        private HolidaySpecification SeniorRaceDay(int year)
         {
             var holidayName = "Senior Race Day";
 
             if (year == 2020)
             {
-                var replacementTTDay = new DateTime(2020, 8, 28);
-                return new Holiday(replacementTTDay, holidayName, holidayName, countryCode, 1978);
+                return new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 28),
+                    EnglishName = holidayName,
+                    LocalName = holidayName,
+                    HolidayTypes = HolidayTypes.Public
+                };
+
+                //var replacementTTDay = new DateTime(year, 8, 28);
+                //return new Holiday(replacementTTDay, holidayName, holidayName, countryCode, 1978);
             }
 
             //The friday after the first saturday of June
             var ttRaceDay = DateSystem.FindDay(DateSystem.FindDay(year, Month.June, DayOfWeek.Saturday, Occurrence.First), DayOfWeek.Friday);
-            return new Holiday(ttRaceDay, holidayName, holidayName, countryCode, 1978);
+            return new HolidaySpecification
+            {
+                Date = ttRaceDay,
+                EnglishName = holidayName,
+                LocalName = holidayName,
+                HolidayTypes = HolidayTypes.Public
+            };
+            //return new Holiday(ttRaceDay, holidayName, holidayName, countryCode, 1978);
         }
 
-        private Holiday EarlyMayBankHoliday(int year, CountryCode countryCode)
+        private HolidaySpecification EarlyMayBankHoliday(int year)
         {
             var holidayName = "Early May Bank Holiday";
 
@@ -84,61 +157,116 @@ namespace Nager.Date.HolidayProviders
             {
                 //https://www.gov.uk/government/news/extra-bank-holiday-to-mark-the-queens-platinum-jubilee-in-2022
                 var secondFridayInMay = DateSystem.FindDay(year, Month.May, DayOfWeek.Friday, Occurrence.Second);
-                return new Holiday(secondFridayInMay, holidayName, holidayName, countryCode, 1978);
+                return new HolidaySpecification
+                {
+                    Date = secondFridayInMay,
+                    EnglishName = holidayName,
+                    LocalName = holidayName,
+                    HolidayTypes = HolidayTypes.Public
+                };
+                //return new Holiday(secondFridayInMay, holidayName, holidayName, countryCode, 1978);
             }
 
             var firstMondayInMay = DateSystem.FindDay(year, Month.May, DayOfWeek.Monday, Occurrence.First);
-            return new Holiday(firstMondayInMay, holidayName, holidayName, countryCode, 1978);
+            return new HolidaySpecification
+            {
+                Date = firstMondayInMay,
+                EnglishName = holidayName,
+                LocalName = holidayName,
+                HolidayTypes = HolidayTypes.Public
+            };
+            //return new Holiday(firstMondayInMay, holidayName, holidayName, countryCode, 1978);
         }
 
-        private Holiday SpringBankHoliday(int year, CountryCode countryCode)
+        private HolidaySpecification SpringBankHoliday(int year)
         {
-            var name = "Spring Bank Holiday";
+            var holidayName = "Spring Bank Holiday";
 
             if (year == 2022)
             {
                 //https://www.gov.uk/government/news/extra-bank-holiday-to-mark-the-queens-platinum-jubilee-in-2022
-                return new Holiday(year, 6, 2, name, name, countryCode);
+                return new HolidaySpecification
+                {
+                    Date = new DateTime(year, 6, 2),
+                    EnglishName = holidayName,
+                    LocalName = holidayName,
+                    HolidayTypes = HolidayTypes.Public
+                };
+                //return new Holiday(year, 6, 2, holidayName, holidayName, countryCode);
             }
 
             var lastMondayInMay = DateSystem.FindLastDay(year, Month.May, DayOfWeek.Monday);
-            return new Holiday(lastMondayInMay, name, name, countryCode);
+            return new HolidaySpecification
+            {
+                Date = lastMondayInMay,
+                EnglishName = holidayName,
+                LocalName = holidayName,
+                HolidayTypes = HolidayTypes.Public
+            };
+            //return new Holiday(lastMondayInMay, holidayName, holidayName, countryCode);
         }
 
 
         #region Royal family
 
-        private Holiday QueensPlatinumJubilee(int year, CountryCode countryCode)
+        private HolidaySpecification QueensPlatinumJubilee(int year)
         {
             if (year == 2022)
             {
+                var holidayName = "Queen’s Platinum Jubilee";
+
                 //Majesty Queen Elizabeth II’s
-                return new Holiday(year, 6, 3, "Queen’s Platinum Jubilee", "Queen’s Platinum Jubilee", countryCode);
+                return new HolidaySpecification
+                {
+                    Date = new DateTime(year, 6, 3),
+                    EnglishName = holidayName,
+                    LocalName = holidayName,
+                    HolidayTypes = HolidayTypes.Public
+                };
+                //return new Holiday(year, 6, 3, "Queen’s Platinum Jubilee", "Queen’s Platinum Jubilee", countryCode);
             }
 
             return null;
         }
 
-        private Holiday QueensStateFuneral(int year, CountryCode countryCode)
+        private HolidaySpecification QueensStateFuneral(int year)
         {
             if (year == 2022)
             {
+                var holidayName = "Queen’s State Funeral";
+
                 //Majesty Queen Elizabeth II’s (https://www.gov.uk/government/news/bank-holiday-announced-for-her-majesty-queen-elizabeth-iis-state-funeral-on-monday-19-september)
-                return new Holiday(year, 9, 19, "Queen’s State Funeral", "Queen’s State Funeral", countryCode);
+                return new HolidaySpecification
+                {
+                    Date = new DateTime(year, 9, 19),
+                    EnglishName = holidayName,
+                    LocalName = holidayName,
+                    HolidayTypes = HolidayTypes.Public
+                };
+                //return new Holiday(year, 9, 19, "Queen’s State Funeral", "Queen’s State Funeral", countryCode);
             }
 
             return null;
         }
 
-        private Holiday CoronationBankHoliday(int year, CountryCode countryCode)
+        private HolidaySpecification CoronationBankHoliday(int year)
         {
             if (year == 2023)
             {
+                var holidayName = "Coronation Bank Holiday";
+
                 //Bank holiday proclaimed in honour of the coronation of His Majesty King Charles III
                 //https://www.gov.uk/government/news/bank-holiday-proclaimed-in-honour-of-the-coronation-of-his-majesty-king-charles-iii
                 //https://www.iomtoday.co.im/news/extra-bank-holiday-to-mark-king-charles-iiis-coronation-571967
 
-                return new Holiday(year, 5, 8, "Coronation Bank Holiday", "Coronation Bank Holiday", countryCode);
+                return new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 8),
+                    EnglishName = holidayName,
+                    LocalName = holidayName,
+                    HolidayTypes = HolidayTypes.Public
+                };
+                //return new Holiday(year, 5, 8, "Coronation Bank Holiday", "Coronation Bank Holiday", countryCode);
             }
 
             return null;
