3f0d6d6434a80dc8f45a26b123c759a390c54926
Tino Hager <tino.hager@nager.at>
2024-03-04 15:47:22 +0100

    optimize console app

--- src/Nager.Date.Console/Program.cs
+++ src/Nager.Date.Console/Program.cs
@@ -19,11 +19,12 @@ if (string.IsNullOrEmpty(countryCode))
 }
 
 Console.WriteLine($"Calculate holidays for {countryCode.ToUpper()} {year}");
-Console.WriteLine("-------------------------------------------------------");
+Console.WriteLine("--------------------------------------------------------------------------------------------------");
+Console.WriteLine("Date         Observed                       English Name                       Local Name   Type");
 
 var publicHolidays = DateSystem.GetPublicHolidays(year, countryCode);
 foreach (var publicHoliday in publicHolidays)
 {
     var counties = publicHoliday.SubdivisionCodes != null ? string.Join(',', publicHoliday.SubdivisionCodes) : "";
-    Console.WriteLine($"{publicHoliday.Date:d}{"",3}{publicHoliday.Name,30}{"",3}{publicHoliday.LocalName,30}{"",3}{publicHoliday.HolidayTypes}{"",3}{counties}");
+    Console.WriteLine($"{publicHoliday.Date:d}{"",3}{publicHoliday.ObservedDate:d}{"",3}{publicHoliday.Name,30}{"",3}{publicHoliday.LocalName,30}{"",3}{publicHoliday.HolidayTypes}{"",3}{counties}");
 }
