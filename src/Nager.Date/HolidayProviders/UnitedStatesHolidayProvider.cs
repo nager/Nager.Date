5a0cfb05e90060901522dc8fe79e521a2b7d2a13
Tino Hager <tino.hager@nager.at>
2024-03-04 15:16:18 +0100

    migrate United States of America

--- src/Nager.Date/HolidayProviders/UnitedStatesHolidayProvider.cs
+++ src/Nager.Date/HolidayProviders/UnitedStatesHolidayProvider.cs
@@ -93,59 +93,163 @@ namespace Nager.Date.HolidayProviders
             var secondMondayInOctober = DateSystem.FindDay(year, Month.October, DayOfWeek.Monday, Occurrence.Second);
             var fourthThursdayInNovember = DateSystem.FindDay(year, Month.November, DayOfWeek.Thursday, Occurrence.Fourth);
 
-            var items = new List<Holiday>();
+            var observedRuleSet = new ObservedRuleSet
+            {
+                Saturday = date => date.AddDays(-1),
+                Sunday = date => date.AddDays(1),
+            };
 
-            #region New Years Day with fallback
-
-            var newYearsDay = new DateTime(year, 1, 1).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
-            items.Add(new Holiday(newYearsDay, "New Year's Day", "New Year's Day", countryCode));
-
-            #endregion
-
-            items.Add(new Holiday(thirdMondayInJanuary, "Martin Luther King, Jr. Day", "Martin Luther King, Jr. Day", countryCode));
-            items.Add(new Holiday(thirdMondayInFebruary, "Presidents Day", "Washington's Birthday", countryCode));
-            items.Add(new Holiday(lastMondayInMay, "Memorial Day", "Memorial Day", countryCode));
-
-            items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode).SetCounties("US-CT", "US-DE", "US-HI", "US-IN", "US-KY", "US-LA", "US-NC", "US-ND", "US-NJ", "US-TN"));
-            items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode).SetType(HolidayTypes.Optional).SetCounties("US-TX"));
-
-            #region Juneteenth
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = observedRuleSet
+                },
+                new HolidaySpecification
+                {
+                    Date = thirdMondayInJanuary,
+                    EnglishName = "Martin Luther King, Jr. Day",
+                    LocalName = "Martin Luther King, Jr. Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = thirdMondayInFebruary,
+                    EnglishName = "Presidents Day",
+                    LocalName = "Washington's Birthday",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = lastMondayInMay,
+                    EnglishName = "Memorial Day",
+                    LocalName = "Memorial Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 7, 4),
+                    EnglishName = "Independence Day",
+                    LocalName = "Independence Day",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = observedRuleSet
+                },
+                new HolidaySpecification
+                {
+                    Date = firstMondayInSeptember,
+                    EnglishName = "Labor Day",
+                    LocalName = "Labour Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = secondMondayInOctober,
+                    EnglishName = "Columbus Day",
+                    LocalName = "Columbus Day",
+                    HolidayTypes = HolidayTypes.Public,
+                    SubdivisionCodes = ["US-AL", "US-AZ", "US-CO", "US-CT", "US-GA", "US-ID", "US-IL", "US-IN", "US-IA", "US-KS", "US-KY", "US-LA", "US-ME", "US-MD", "US-MA", "US-MS", "US-MO", "US-MT", "US-NE", "US-NH", "US-NJ", "US-NM", "US-NY", "US-NC", "US-OH", "US-OK", "US-PA", "US-RI", "US-SC", "US-TN", "US-UT", "US-VA", "US-WV"]
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 11, 11),
+                    EnglishName = "Veterans Day",
+                    LocalName = "Veterans Day",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = observedRuleSet
+                },
+                new HolidaySpecification
+                {
+                    Date = fourthThursdayInNovember,
+                    EnglishName = "Thanksgiving Day",
+                    LocalName = "Thanksgiving Day",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = observedRuleSet
+                },
+                this._catholicProvider.GoodFriday("Good Friday", year).SetSubdivisionCodes("US-CT", "US-DE", "US-HI", "US-IN", "US-KY", "US-LA", "US-NC", "US-ND", "US-NJ", "US-TN"),
+                this._catholicProvider.GoodFriday("Good Friday", year).SetSubdivisionCodes("US-TX").SetHolidayTypes(HolidayTypes.Optional)
+            };
 
             if (year >= 2021)
             {
-                var juneteenth = new DateTime(year, 6, 19).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
-                items.Add(new Holiday(juneteenth, "Juneteenth", "Juneteenth", countryCode, 2021));
+                holidaySpecifications.AddIfNotNull(new HolidaySpecification
+                {
+                    Date = new DateTime(year, 6, 19),
+                    EnglishName = "Juneteenth",
+                    LocalName = "Juneteenth",
+                    HolidayTypes = HolidayTypes.Public,
+                    ObservedRuleSet = observedRuleSet
+                });
+
+                //var juneteenth = new DateTime(year, 6, 19).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
+                //items.Add(new Holiday(juneteenth, "Juneteenth", "Juneteenth", countryCode, 2021));
             }
 
-            #endregion
+            var holidays = HolidaySpecificationProcessor.Process(holidaySpecifications, countryCode);
+            return holidays.OrderBy(o => o.Date);
 
-            #region Independence Day with fallback
 
-            var independenceDay = new DateTime(year, 7, 4).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
-            items.Add(new Holiday(independenceDay, "Independence Day", "Independence Day", countryCode));
+            //var items = new List<Holiday>();
 
-            #endregion
+            //#region New Years Day with fallback
+            //var newYearsDay = new DateTime(year, 1, 1).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
+            //items.Add(new Holiday(newYearsDay, "New Year's Day", "New Year's Day", countryCode));
+            //#endregion
 
-            items.Add(new Holiday(firstMondayInSeptember, "Labor Day", "Labour Day", countryCode));
-            items.Add(new Holiday(secondMondayInOctober, "Columbus Day", "Columbus Day", countryCode, null, new string[] { "US-AL", "US-AZ", "US-CO", "US-CT", "US-GA", "US-ID", "US-IL", "US-IN", "US-IA", "US-KS", "US-KY", "US-LA", "US-ME", "US-MD", "US-MA", "US-MS", "US-MO", "US-MT", "US-NE", "US-NH", "US-NJ", "US-NM", "US-NY", "US-NC", "US-OH", "US-OK", "US-PA", "US-RI", "US-SC", "US-TN", "US-UT", "US-VA", "US-WV" }));
+            //items.Add(new Holiday(thirdMondayInJanuary, "Martin Luther King, Jr. Day", "Martin Luther King, Jr. Day", countryCode));
+            //items.Add(new Holiday(thirdMondayInFebruary, "Presidents Day", "Washington's Birthday", countryCode));
+            //items.Add(new Holiday(lastMondayInMay, "Memorial Day", "Memorial Day", countryCode));
 
-            #region Veterans Day with fallback
+            //items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode).SetCounties("US-CT", "US-DE", "US-HI", "US-IN", "US-KY", "US-LA", "US-NC", "US-ND", "US-NJ", "US-TN"));
+            //items.Add(this._catholicProvider.GoodFriday("Good Friday", year, countryCode).SetType(HolidayTypes.Optional).SetCounties("US-TX"));
 
-            var veteransDay = new DateTime(year, 11, 11).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
-            items.Add(new Holiday(veteransDay, "Veterans Day", "Veterans Day", countryCode));
+            //#region Juneteenth
 
-            #endregion
+            //if (year >= 2021)
+            //{
+            //    var juneteenth = new DateTime(year, 6, 19).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
+            //    items.Add(new Holiday(juneteenth, "Juneteenth", "Juneteenth", countryCode, 2021));
+            //}
 
-            items.Add(new Holiday(fourthThursdayInNovember, "Thanksgiving Day", "Thanksgiving Day", countryCode, 1863));
+            //#endregion
 
-            #region Christmas Day with fallback
+            //#region Independence Day with fallback
 
-            var christmasDay = new DateTime(year, 12, 25).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
-            items.Add(new Holiday(christmasDay, "Christmas Day", "Christmas Day", countryCode));
+            //var independenceDay = new DateTime(year, 7, 4).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
+            //items.Add(new Holiday(independenceDay, "Independence Day", "Independence Day", countryCode));
 
-            #endregion
+            //#endregion
 
-            return items.OrderBy(o => o.Date);
+            //items.Add(new Holiday(firstMondayInSeptember, "Labor Day", "Labour Day", countryCode));
+            //items.Add(new Holiday(secondMondayInOctober, "Columbus Day", "Columbus Day", countryCode, null, new string[] { "US-AL", "US-AZ", "US-CO", "US-CT", "US-GA", "US-ID", "US-IL", "US-IN", "US-IA", "US-KS", "US-KY", "US-LA", "US-ME", "US-MD", "US-MA", "US-MS", "US-MO", "US-MT", "US-NE", "US-NH", "US-NJ", "US-NM", "US-NY", "US-NC", "US-OH", "US-OK", "US-PA", "US-RI", "US-SC", "US-TN", "US-UT", "US-VA", "US-WV" }));
+
+            //#region Veterans Day with fallback//
+
+            //var veteransDay = new DateTime(year, 11, 11).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
+            //items.Add(new Holiday(veteransDay, "Veterans Day", "Veterans Day", countryCode));
+
+            //#endregion
+
+            //items.Add(new Holiday(fourthThursdayInNovember, "Thanksgiving Day", "Thanksgiving Day", countryCode, 1863));
+
+            //#region Christmas Day with fallback
+
+            //var christmasDay = new DateTime(year, 12, 25).Shift(saturday => saturday.AddDays(-1), sunday => sunday.AddDays(1));
+            //items.Add(new Holiday(christmasDay, "Christmas Day", "Christmas Day", countryCode));
+
+            //#endregion
+
+            //return items.OrderBy(o => o.Date);
         }
 
         /// <inheritdoc/>
