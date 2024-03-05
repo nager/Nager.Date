0758f64e6fa3791a8d82a8df5aac9d7550e58d8b
Martin Stühmer <me@samtrion.net>
2024-03-05 16:10:00 +0100

    Introduction of architecture tests for stabilization and standardization of implementation.
    With this particular form of unit tests, it is possible to normalize the architecture of the application and establish standards, thereby fundamentally improving the quality of the application.

---
+++ src/Nager.Date.ArchitectureTest/IHolidayProviderArchitectureTests.cs
@@ -0,0 +1,54 @@
+using ArchUnitNET.Domain;
+using ArchUnitNET.MSTestV2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nager.Date.ArchitectureTest.Internals;
+using Nager.Date.HolidayProviders;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace Nager.Date.ArchitectureTest
+{
+    [TestClass]
+    public class IHolidayProviderArchitectureTests
+    {
+        private static readonly IObjectProvider<Class> _holidayProviders = Classes()
+            .That()
+            .AreAssignableTo(typeof(IHolidayProvider))
+            .As("Nager.Date HolidayProvider");
+
+        [TestMethod]
+        public void HolidayProvider_Should_Be_Internal()
+        {
+            var rule = Classes()
+                .That()
+                .Are(_holidayProviders)
+                .Should()
+                .BeInternal();
+
+            rule.Check(NagerDateArchitecture.Instance);
+        }
+
+        [TestMethod]
+        public void HolidayProvider_Should_Be_Sealed()
+        {
+            var rule = Classes()
+                .That()
+                .Are(_holidayProviders)
+                .Should()
+                .BeSealed();
+
+            rule.Check(NagerDateArchitecture.Instance);
+        }
+
+        [TestMethod]
+        public void HolidayProvider_Should_Reside_In_Namespace()
+        {
+            var rule = Classes()
+                .That()
+                .Are(_holidayProviders)
+                .Should()
+                .ResideInNamespace("Nager.Date.HolidayProviders");
+
+            rule.Check(NagerDateArchitecture.Instance);
+        }
+    }
+}
