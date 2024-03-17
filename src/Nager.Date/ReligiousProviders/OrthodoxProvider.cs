bb186ebcf4b893735a485c1ae7c2b98570f2decd
Tino Hager <tino.hager@nager.at>
2024-03-18 00:03:38 +0100

    Fix Othodox Easter Sunday calculation (#619)

--- src/Nager.Date/ReligiousProviders/OrthodoxProvider.cs
+++ src/Nager.Date/ReligiousProviders/OrthodoxProvider.cs
@@ -14,21 +14,38 @@ namespace Nager.Date.ReligiousProviders
         /// <inheritdoc/>
         public DateTime EasterSunday(int year)
         {
+            var daysToAddForGregorianCalendar = year switch
+            {
+                int y when (y >= 1583 && y <= 1699) => 10,
+                int y when (y >= 1700 && y <= 1799) => 11,
+                int y when (y >= 1800 && y <= 1899) => 12,
+                int y when (y >= 1900 && y <= 2099) => 13,
+                int y when (y >= 2100 && y <= 2199) => 14,
+                int y when (y >= 2200 && y <= 2299) => 15,
+                int y when (y >= 2300 && y <= 2499) => 16,
+                int y when (y >= 2500 && y <= 2599) => 17,
+                int y when (y >= 2600 && y <= 2699) => 18,
+                int y when (y >= 2700 && y <= 2899) => 19,
+                int y when (y >= 2900 && y <= 2999) => 20,
+                int y when (y >= 3000 && y <= 3099) => 21,
+                int y when (y >= 3100 && y <= 3299) => 22,
+                int y when (y >= 3300 && y <= 3399) => 23,
+                _ => 0,
+            };
+
             return _cache.GetOrAdd(year, y =>
             {
                 var a = y % 19;
                 var b = y % 7;
                 var c = y % 4;
 
-                var d = (19 * a + 16) % 30;
-                var e = (2 * c + 4 * b + 6 * d) % 7;
-                var f = (19 * a + 16) % 30;
-                var key = f + e + 3;
+                var d = (19 * a + 15) % 30;
+                var e = (2 * c + 4 * b - d + 34) % 7;
 
-                var month = key > 30 ? 5 : 4;
-                var day = key > 30 ? key - 30 : key;
+                var month = (int)Math.Floor((d + e + 114.0) / 31);
+                var day = ((d + e + 114) % 31) + 1;
 
-                return new DateTime(y, month, day);
+                return new DateTime(y, month, day).AddDays(daysToAddForGregorianCalendar);
             });
         }
 
