ee1e1e79128cd5f8e092ea18204fe24deb7f5380
Slaega <88974755+SedeoLeos@users.noreply.github.com>
2025-05-09 09:54:09 +0100

    🇨🇩 Add Holiday Provider for Democratic Republic of the Congo (CD) (#798)
    * feat(cd): add official public holidays for DR Congo (RDC)
    
    Includes fixed-date holidays such as Martyrs' Day, Kimbangu's Day,
    Independence Day, and Parents' Day. Corrected country code to CD.
    Cleaned up duplicated entries and formatting issues.
    - #796
    
    * fix(cd): Corrected the country code from CG to CD
    
    * fix(cd): remove All Saints' Day
    
    * Update DemocraticRepublicOfCongoHolidayProvider.cs
    
    ---------
    
    Co-authored-by: Tino Hager <tino.hager@nager.at>

---
+++ src/Nager.Date/HolidayProviders/DemocraticRepublicOfCongoHolidayProvider.cs
@@ -0,0 +1,116 @@
+using Nager.Date.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Nager.Date.HolidayProviders
+{
+    /// <summary>
+    /// Democratic Republic of the Congo HolidayProvider
+    /// </summary>
+    internal sealed class DemocraticRepublicOfCongoHolidayProvider : AbstractHolidayProvider
+    {
+        /// <summary>
+        /// Democratic Republic of the Congo HolidayProvider
+        /// </summary>
+        public DemocraticRepublicOfCongoHolidayProvider() : base(CountryCode.CD)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<HolidaySpecification> GetHolidaySpecifications(int year)
+        {
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 4),
+                    EnglishName = "Martyrs Day",
+                    LocalName = "Martyrs Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 16),
+                    EnglishName = "Laurent-Désiré Kabila Assassination",
+                    LocalName = "Laurent-Désiré Kabila Assassination",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 1, 17),
+                    EnglishName = "Patrice Lumumba Assassination",
+                    LocalName = "Patrice Lumumba Assassination",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 4, 6),
+                    EnglishName = "Kimbangu's Day",
+                    LocalName = "Kimbangu's Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Labour Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 5, 17),
+                    EnglishName = "Liberation Day",
+                    LocalName = "Liberation Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 6, 30),
+                    EnglishName = "Independence Day",
+                    LocalName = "Independence Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 1),
+                    EnglishName = "Parents' Day",
+                    LocalName = "Parents' Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 8, 2),
+                    EnglishName = "Congolese Genocide Day",
+                    LocalName = "Congolese Genocide Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public,
+                }
+            };
+
+            return holidaySpecifications;
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<string> GetSources()
+        {
+            return
+            [
+                "https://en.wikipedia.org/wiki/Public_holidays_in_the_Democratic_Republic_of_the_Congo",
+                "https://www.officeholidays.com/countries/dr-congo"
+            ];
+        }
+    }
+}
