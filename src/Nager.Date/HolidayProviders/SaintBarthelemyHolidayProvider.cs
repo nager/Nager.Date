caa5505f1804b1da2548730e44e6b4fcf442c966
Tino Hager <tino.hager@nager.at>
2026-06-17 21:47:30 +0200

    Saint Barthélemy - Add holiday provider

---
+++ src/Nager.Date/HolidayProviders/SaintBarthelemyHolidayProvider.cs
@@ -0,0 +1,127 @@
+using Nager.Date.Extensions;
+using Nager.Date.Helpers;
+using Nager.Date.Models;
+using Nager.Date.ReligiousProviders;
+using System;
+using System.Collections.Generic;
+
+namespace Nager.Date.HolidayProviders
+{
+    /// <summary>
+    /// Saint Barthélemy HolidayProvider
+    /// </summary>
+    internal sealed class SaintBarthelemyHolidayProvider : AbstractHolidayProvider
+    {
+        private readonly ICatholicProvider _catholicProvider;
+
+        /// <summary>
+        /// Saint Barthélemy HolidayProvider
+        /// </summary>
+        /// <param name="catholicProvider"></param>
+        public SaintBarthelemyHolidayProvider(
+            ICatholicProvider catholicProvider) : base(CountryCode.BL)
+        {
+            this._catholicProvider = catholicProvider;
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<HolidaySpecification> GetHolidaySpecifications(int year)
+        {
+            var sovereigntyDayDate = DateHelper.FindDay(year, Month.June, DayOfWeek.Saturday, Occurrence.Second).AddDays(2);
+            var firstMondayInAugust = DateHelper.FindDay(year, Month.August, DayOfWeek.Monday, Occurrence.First);
+            var augustThursdayDate = firstMondayInAugust.AddDays(3);
+            var constitutionDayDate = firstMondayInAugust.AddDays(4);
+
+
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Id = "NEWYEARSDAY-01",
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "Jour de l'An",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "LABOURDAY-01",
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Fête du Travail",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "VICTORYDAY-01",
+                    Date = new DateTime(year, 5, 8),
+                    EnglishName = "Victory Day",
+                    LocalName = "Fête de la Victoire 1945",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "NATIONALDAY-01",
+                    Date = new DateTime(year, 7, 14),
+                    EnglishName = "National Day",
+                    LocalName = "Fête Nationale",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Id = "ASSUMPTIONDAY-01",
+                    Date = new DateTime(year, 8, 15),
+                    EnglishName = "Assumption Day",
+                    LocalName = "Assomption",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Id = "ABOLITIONDAY-01",
+                    Date = new DateTime(year, 10, 9),
+                    EnglishName = "Abolition Day",
+                    LocalName = "Abolition de l'Esclavage",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Id = "ALLSAINTSDAY-01",
+                    Date = new DateTime(year, 11, 1),
+                    EnglishName = "All Saints' Day",
+                    LocalName = "Toussaint",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Id = "ARMISTICEDAY-01",
+                    Date = new DateTime(year, 11, 11),
+                    EnglishName = "Armistice Day",
+                    LocalName = "Armistice",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "CHRISTMASDAY-01",
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Noël",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                this._catholicProvider.EasterMonday("Lundi de Pâques", year),
+                this._catholicProvider.AscensionDay("Ascension", year),
+                this._catholicProvider.WhitMonday("Lundi de Pentecôte", year),
+            };
+
+            return holidaySpecifications;
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<string> GetSources()
+        {
+            return
+            [
+                "https://en.wikipedia.org/wiki/Public_holidays_in_Saint_Barth%C3%A9lemy",
+            ];
+        }
+    }
+}
