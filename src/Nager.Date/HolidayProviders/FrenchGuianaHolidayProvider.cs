d33e56b78133ff160b71c0ca302063db9c9c6cfc
Tino Hager <tino.hager@nager.at>
2026-06-19 10:05:15 +0200

    French Guiana - Add holiday provider

---
+++ src/Nager.Date/HolidayProviders/FrenchGuianaHolidayProvider.cs
@@ -0,0 +1,127 @@
+using Nager.Date.Models;
+using Nager.Date.ReligiousProviders;
+using System;
+using System.Collections.Generic;
+
+namespace Nager.Date.HolidayProviders
+{
+    /// <summary>
+    /// French Guiana HolidayProvider
+    /// </summary>
+    internal sealed class FrenchGuianaHolidayProvider : AbstractHolidayProvider
+    {
+        private readonly ICatholicProvider _catholicProvider;
+
+        /// <summary>
+        /// French Guiana HolidayProvider
+        /// </summary>
+        /// <param name="catholicProvider"></param>
+        public FrenchGuianaHolidayProvider(
+            ICatholicProvider catholicProvider) : base(CountryCode.GF)
+        {
+            this._catholicProvider = catholicProvider;
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<HolidaySpecification> GetHolidaySpecifications(int year)
+        {
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Id = "NEWYEARSDAY-01",
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "Jour de l'An",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "LABOURDAY-01",
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Fête du Travail",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "VICTORYDAY-01",
+                    Date = new DateTime(year, 5, 8),
+                    EnglishName = "Victory Day",
+                    LocalName = "Fête de la Victoire 1945",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "EMANCIPATIONDAY-01",
+                    Date = new DateTime(year, 6, 10),
+                    EnglishName = "Emancipation Day",
+                    LocalName = "Abolition de l'Esclavage",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "NATIONALDAY-01",
+                    Date = new DateTime(year, 7, 14),
+                    EnglishName = "National Day",
+                    LocalName = "Fête Nationale",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "ASSUMPTIONDAY-01",
+                    Date = new DateTime(year, 8, 15),
+                    EnglishName = "Assumption Day",
+                    LocalName = "Assomption",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                //new HolidaySpecification
+                //{
+                //    Id = "CAYENNEFESTIVAL-01",
+                //    Date = new DateTime(year, 10, 15),
+                //    EnglishName = "Cayenne Festival",
+                //    LocalName = "Festival Cayenne",
+                //    HolidayTypes = HolidayTypes.Public
+                //},
+                new HolidaySpecification
+                {
+                    Id = "ALLSAINTSDAY-01",
+                    Date = new DateTime(year, 11, 1),
+                    EnglishName = "All Saints' Day",
+                    LocalName = "Toussaint",
+                    HolidayTypes = HolidayTypes.Public
+                },
+                new HolidaySpecification
+                {
+                    Id = "ARMISTICEDAY-01",
+                    Date = new DateTime(year, 11, 11),
+                    EnglishName = "Armistice Day",
+                    LocalName = "Armistice de 1918",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "CHRISTMASDAY-01",
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Noël",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                this._catholicProvider.EasterMonday("Lundi de Pâques", year),
+                this._catholicProvider.AscensionDay("Ascension", year),
+                this._catholicProvider.WhitMonday("Lundi de Pentecôte", year),
+            };
+
+            return holidaySpecifications;
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<string> GetSources()
+        {
+            return
+            [
+                "https://en.wikipedia.org/wiki/Public_holidays_in_French_Guiana",
+            ];
+        }
+    }
+}
