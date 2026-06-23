538fccb71e0e0d573d64f8d675d7516a6795e77c
Tino Hager <tino.hager@nager.at>
2026-06-23 08:35:48 +0200

    Trinidad and Tobago - Add holiday provider

---
+++ src/Nager.Date/HolidayProviders/TrinidadAndTobagoHolidayProvider.cs
@@ -0,0 +1,119 @@
+using Nager.Date.Models;
+using Nager.Date.ReligiousProviders;
+using System;
+using System.Collections.Generic;
+
+namespace Nager.Date.HolidayProviders
+{
+    /// <summary>
+    /// Trinidad and Tobago HolidayProvider
+    /// </summary>
+    internal sealed class TrinidadAndTobagoHolidayProvider : AbstractHolidayProvider
+    {
+        private readonly ICatholicProvider _catholicProvider;
+
+        /// <summary>
+        /// Trinidad and Tobago HolidayProvider
+        /// </summary>
+        /// <param name="catholicProvider"></param>
+        public TrinidadAndTobagoHolidayProvider(
+            ICatholicProvider catholicProvider) : base(CountryCode.TT)
+        {
+            this._catholicProvider = catholicProvider;
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<HolidaySpecification> GetHolidaySpecifications(int year)
+        {
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Id = "NEWYEARSDAY-01",
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "SPIRITUALBAPTISTDAY-01",
+                    Date = new DateTime(year, 3, 30),
+                    EnglishName = "Spiritual Baptist Shouter Liberation Day",
+                    LocalName = "Spiritual Baptist Shouter Liberation Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "INDIANARRIVALDAY-01",
+                    Date = new DateTime(year, 5, 30),
+                    EnglishName = "Indian Arrival Day",
+                    LocalName = "Indian Arrival Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "LABOURDAY-01",
+                    Date = new DateTime(year, 6, 19),
+                    EnglishName = "Labour Day",
+                    LocalName = "Labour Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "EMANCIPATIONDAY-01",
+                    Date = new DateTime(year, 8, 1),
+                    EnglishName = "Emancipation Day",
+                    LocalName = "Emancipation Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "INDEPENDENCEDAY-01",
+                    Date = new DateTime(year, 8, 31),
+                    EnglishName = "Independence Day",
+                    LocalName = "Independence Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "REPUBLICDAY-01",
+                    Date = new DateTime(year, 9, 24),
+                    EnglishName = "Republic Day",
+                    LocalName = "Republic Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "CHRISTMASDAY-01",
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "STSTEPHENSDAY-01",
+                    Date = new DateTime(year, 12, 26),
+                    EnglishName = "St. Stephen's Day",
+                    LocalName = "Boxing Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                this._catholicProvider.GoodFriday("Good Friday", year),
+                this._catholicProvider.EasterMonday("Easter Monday", year),
+                this._catholicProvider.CorpusChristi("Corpus Christi", year),
+            };
+
+            return holidaySpecifications;
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<string> GetSources()
+        {
+            return
+            [
+                "https://en.wikipedia.org/wiki/Public_holidays_in_Trinidad_and_Tobago",
+            ];
+        }
+    }
+}
