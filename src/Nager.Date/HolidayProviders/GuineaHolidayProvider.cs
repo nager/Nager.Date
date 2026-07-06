bc14618ded4532c1b4c2fff5cf1cac172a042052
Tino Hager <tino.hager@nager.at>
2026-07-06 21:00:30 +0200

    Guinea - Add holiday provider

---
+++ src/Nager.Date/HolidayProviders/GuineaHolidayProvider.cs
@@ -0,0 +1,102 @@
+using Nager.Date.Models;
+using Nager.Date.ReligiousProviders;
+using System;
+using System.Collections.Generic;
+
+namespace Nager.Date.HolidayProviders
+{
+    /// <summary>
+    /// Guinea HolidayProvider
+    /// </summary>
+    internal sealed class GuineaHolidayProvider : AbstractHolidayProvider
+    {
+        private readonly ICatholicProvider _catholicProvider;
+
+        /// <summary>
+        /// Guinea HolidayProvider
+        /// </summary>
+        /// <param name="catholicProvider"></param>
+        public GuineaHolidayProvider(
+            ICatholicProvider catholicProvider) : base(CountryCode.GN)
+        {
+            this._catholicProvider = catholicProvider;
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<HolidaySpecification> GetHolidaySpecifications(int year)
+        {
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Id = "NEWYEARSDAY-01",
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "LABOURDAY-01",
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Labour Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "AFRICADAY-01",
+                    Date = new DateTime(year, 5, 25),
+                    EnglishName = "Africa Day",
+                    LocalName = "Africa Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "ASSUMPTIONDAY-01",
+                    Date = new DateTime(year, 8, 15),
+                    EnglishName = "Assumption Day",
+                    LocalName = "Assumption Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "INDEPENDENCEDAY-01",
+                    Date = new DateTime(year, 10, 2),
+                    EnglishName = "Independence Day",
+                    LocalName = "Independence Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "CHRISTMASDAY-01",
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                this._catholicProvider.EasterMonday("Easter Monday", year),
+            };
+
+            //TODO: Islamic Holidays
+            //CalendarType: Lunar
+            //DeterminationType: MoonSighting
+            //Astronomical calculation: No
+            //12 Rabi' al-awwal	Day after the Prophet's Birthday	Birthday of Muhammad
+            //27 Ramadan	Day after Lailatou-Kadr	Revelation of the Qur'an
+            //1 Shawwal	Korité	Breaking of the Ramadan fast
+            //10 Dhu al-Hijjah	Tabaski	Feast of the Sacrifice of Abraham
+
+            return holidaySpecifications;
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<string> GetSources()
+        {
+            return
+            [
+                "https://en.wikipedia.org/wiki/Public_holidays_in_Guinea",
+            ];
+        }
+    }
+}
