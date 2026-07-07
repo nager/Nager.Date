a403ecab400ee8960f0212f49007b84fd151ff87
Tino Hager <tino.hager@nager.at>
2026-07-07 09:11:27 +0200

    Senegal - Add holiday provider

---
+++ src/Nager.Date/HolidayProviders/SenegalHolidayProvider.cs
@@ -0,0 +1,105 @@
+using Nager.Date.Models;
+using Nager.Date.ReligiousProviders;
+using System;
+using System.Collections.Generic;
+
+namespace Nager.Date.HolidayProviders
+{
+    /// <summary>
+    /// Senegal HolidayProvider
+    /// </summary>
+    internal sealed class SenegalHolidayProvider : AbstractHolidayProvider
+    {
+        private readonly ICatholicProvider _catholicProvider;
+
+        /// <summary>
+        /// Senegal HolidayProvider
+        /// </summary>
+        /// <param name="catholicProvider"></param>
+        public SenegalHolidayProvider(
+            ICatholicProvider catholicProvider) : base(CountryCode.SN)
+        {
+            this._catholicProvider = catholicProvider;
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<HolidaySpecification> GetHolidaySpecifications(int year)
+        {
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Id = "NEWYEARSDAY-01",
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "INDEPENDENCEDAY-01",
+                    Date = new DateTime(year, 4, 4),
+                    EnglishName = "Independence Day",
+                    LocalName = "Independence Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "LABOURDAY-01",
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Labour Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "ASSUMPTIONDAY-01",
+                    Date = new DateTime(year, 8, 15),
+                    EnglishName = "Assumption Day",
+                    LocalName = "Assumption Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "ALLSAINTSDAY-01",
+                    Date = new DateTime(year, 11, 1),
+                    EnglishName = "All Saints' Day",
+                    LocalName = "All Saints' Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "CHRISTMASDAY-01",
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                this._catholicProvider.EasterMonday("Easter Monday", year),
+                this._catholicProvider.WhitMonday("Whit Monday", year),
+                this._catholicProvider.AscensionDay("Ascension Day", year),
+            };
+
+            //TODO: Islamic Holidays
+            //CalendarType: Lunar
+            //DeterminationType: MoonSighting
+            //Astronomical calculation: No
+            //18 Safar	Grand Magal of Touba	The Grand Magal (pilgrimage) is the most important religious festival of the Senegalese Muslim brotherhood, the Mourides.[2]
+            //1 Shawwal	Korité	End of Ramadan, Muslim Breaking of the Fast
+            //10 Dhu al-Hijjah	Tabaski	Muslim Feast of the Sacrifice of the prophet Abraham
+            //12 Rabi' al-awwal	Mawlid	Prophet Mohammed's Birthday
+            //10 Muharram	Tamkharit	Commemorates the victory of Prophet Moses over the pharaohs.
+
+            return holidaySpecifications;
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<string> GetSources()
+        {
+            return
+            [
+                "https://en.wikipedia.org/wiki/Public_holidays_in_Senegal",
+            ];
+        }
+    }
+}
