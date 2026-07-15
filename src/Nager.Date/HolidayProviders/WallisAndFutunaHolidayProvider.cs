452e0505a9af499edff99086dd2343dd9839934b
Tino Hager <tino.hager@nager.at>
2026-07-15 21:28:43 +0200

    Wallis and Futuna - Add holiday provider (#1111)

---
+++ src/Nager.Date/HolidayProviders/WallisAndFutunaHolidayProvider.cs
@@ -0,0 +1,135 @@
+using Nager.Date.Models;
+using Nager.Date.ReligiousProviders;
+using System;
+using System.Collections.Generic;
+
+namespace Nager.Date.HolidayProviders
+{
+    /// <summary>
+    /// Wallis and Futuna HolidayProvider
+    /// </summary>
+    internal sealed class WallisAndFutunaHolidayProvider : AbstractHolidayProvider
+    {
+        private readonly ICatholicProvider _catholicProvider;
+
+        /// <summary>
+        /// Wallis and Futuna HolidayProvider
+        /// </summary>
+        /// <param name="catholicProvider"></param>
+        public WallisAndFutunaHolidayProvider(
+            ICatholicProvider catholicProvider) : base(CountryCode.WF)
+        {
+            this._catholicProvider = catholicProvider;
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<HolidaySpecification> GetHolidaySpecifications(int year)
+        {
+            var holidaySpecifications = new List<HolidaySpecification>
+            {
+                new HolidaySpecification
+                {
+                    Id = "NEWYEARSDAY-01",
+                    Date = new DateTime(year, 1, 1),
+                    EnglishName = "New Year's Day",
+                    LocalName = "New Year's Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "SAINTPIERRECHANELDAY-01",
+                    Date = new DateTime(year, 4, 28),
+                    EnglishName = "Saint Pierre-Chanel Day",
+                    LocalName = "Saint Pierre-Chanel Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "LABOURDAY-01",
+                    Date = new DateTime(year, 5, 1),
+                    EnglishName = "Labour Day",
+                    LocalName = "Labour Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "VICTORYDAY-01",
+                    Date = new DateTime(year, 5, 8),
+                    EnglishName = "Victory Dayy",
+                    LocalName = "Victory Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "SAINTSPETERANDPAUL-01",
+                    Date = new DateTime(year, 6, 29),
+                    EnglishName = "Saints Peter and Paul",
+                    LocalName = "Saints Peter and Paul",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "BASTILLEDAY-01",
+                    Date = new DateTime(year, 7, 14),
+                    EnglishName = "Bastille Day",
+                    LocalName = "Bastille Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "TERRITORYDAY-01",
+                    Date = new DateTime(year, 7, 29),
+                    EnglishName = "Territory Day",
+                    LocalName = "Territory Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "ASSUMPTIONDAY-01",
+                    Date = new DateTime(year, 8, 15),
+                    EnglishName = "Assumption Day",
+                    LocalName = "Assumption Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "ALLSAINTSDAY-01",
+                    Date = new DateTime(year, 11, 1),
+                    EnglishName = "All Saints' Day",
+                    LocalName = "All Saints' Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "ARMISTICEDAY-01",
+                    Date = new DateTime(year, 11, 11),
+                    EnglishName = "Armistice Day",
+                    LocalName = "Armistice Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                new HolidaySpecification
+                {
+                    Id = "CHRISTMASDAY-01",
+                    Date = new DateTime(year, 12, 25),
+                    EnglishName = "Christmas Day",
+                    LocalName = "Christmas Day",
+                    HolidayTypes = HolidayTypes.Public,
+                },
+                this._catholicProvider.EasterMonday("Easter Monday", year),
+                this._catholicProvider.AscensionDay("Ascension Day", year),
+                this._catholicProvider.WhitMonday("Whit Monday", year),
+            };
+
+            return holidaySpecifications;
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<string> GetSources()
+        {
+            return
+            [
+                "https://en.wikipedia.org/wiki/Public_holidays_in_Wallis_and_Futuna",
+            ];
+        }
+    }
+}
